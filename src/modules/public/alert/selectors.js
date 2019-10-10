91bf09b62a50f44bba3f575e0d3d003d6cc9a3c3
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:16:00 +0100

    Update selectors.js

--- src/modules/public/alert/selectors.js
+++ src/modules/public/alert/selectors.js
@@ -1,1 +1,13 @@
-export const selectAlertState = (state) => state.public.alerts;
+import { RootState } from '../../index';
+import { AlertState } from './reducer';
+
+export const selectAlertState = (state: RootState): AlertState => state.public.alerts;
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/alert/selectors.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/alert/selectors.ts
