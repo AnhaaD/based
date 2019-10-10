ec4b3746edf43c87c6109ceed94dfcd076494ca0
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:26:05 +0100

    Update actions.js

--- src/modules/public/i18n/actions.js
+++ src/modules/public/i18n/actions.js
@@ -1,9 +1,21 @@
-import { CHANGE_LANGUAGE, } from './constants';
+import {
+    CHANGE_LANGUAGE,
+} from './constants';
 
-export const changeLanguage = (payload) => ({
-
+export interface ChangeLanguageAction {
+    type: string;
+    payload: string;
+}
+export const changeLanguage = (payload: string): ChangeLanguageAction => ({
     type: CHANGE_LANGUAGE,
-
     payload,
-
 });
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/i18n/actions.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/i18n/actions.ts
