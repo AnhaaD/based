bf27c48165d33e5e30b5f93bd774bbaaed961268
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:21:42 +0100

    Create index.js

---
+++ src/modules/public/currencies/index.js
@@ -0,0 +1,14 @@
+export * from './actions';
+export * from './sagas/currenciesFetchSaga';
+export * from './reducer';
+export * from './selectors';
+export * from './types';
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/currencies/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/currencies/index.ts
