6bdca3f77a7b18ed9b3477300d21017e550c8f4e
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:27:20 +0100

    Update reducer.js

--- src/modules/public/i18n/reducer.js
+++ src/modules/public/i18n/reducer.js
@@ -1,43 +1,46 @@
-import { CHANGE_LANGUAGE, } from './constants';
+import {
+    CHANGE_LANGUAGE,
+} from './constants';
 
-import { languageMap } from '../../../translations';
+import { customLanguageMap } from '../../../custom/translations';
+import {
+    ChangeLanguageAction,
+} from './actions';
+
+export interface LanguageState {
+    lang: string;
+    messages: { [pair: string]: string };
+}
 
 const defaultLanguage = {
-
     code: 'en',
-
 };
 
-const currentLang = localStorage.getItem('lang_code') || defaultLanguage.code;
+const currentLang: string = localStorage.getItem('lang_code') || defaultLanguage.code;
 
-export const initialChangeLanguageState = {
-
+export const initialChangeLanguageState: LanguageState = {
     lang: currentLang,
-
-    messages: languageMap[currentLang],
-
+    messages: customLanguageMap[currentLang],
 };
 
-export const changeLanguageReducer = (state = initialChangeLanguageState, action) => {
-
+export const changeLanguageReducer = (state = initialChangeLanguageState, action: ChangeLanguageAction) => {
     switch (action.type) {
-
         case CHANGE_LANGUAGE:
-
             localStorage.setItem('lang_code', action.payload);
-
             return {
-
                 lang: action.payload,
-
-                messages: languageMap[action.payload],
-
-            };
-
+                messages: customLanguageMap[action.payload],
+             };
         default:
-
             return state;
-
     }
-
 };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/i18n/reducer.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/i18n/reducer.ts
