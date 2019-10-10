327ce2316e46f6f7c445213ff9d92c56eb27e0aa
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:52:09 +0100

    Update index.js

--- src/containers/ToolBar/ProgressLabel/index.js
+++ src/containers/ToolBar/ProgressLabel/index.js
@@ -1,42 +1,39 @@
 import classnames from 'classnames';
-
 import * as React from 'react';
 
-export class ProgressLabel extends React.Component {
+interface Props {
+    progress?: string;
+    isPositive?: boolean;
+    additional?: string;
+    bidUnit?: string;
+}
 
-    render() {
-
-        const { progress, isPositive, additional, bidUnit, } = this.props;
-
+export class ProgressLabel extends React.Component<Props>{
+    public render() {
+        const {
+            progress,
+            isPositive,
+            additional,
+            bidUnit,
+        } = this.props;
         const className = classnames({
-
             'pg-trading-header-progress-label-progress-positive': isPositive,
-
             'pg-trading-header-progress-label-progress-negative': !isPositive,
-
         });
-
-        return (React.createElement("div", { className: "pg-trading-header-progress-label" },
-
-            React.createElement("div", { className: className },
-
-                progress,
-
-                " ",
-
-                bidUnit),
-
-            React.createElement("div", { className: "pg-trading-header-progress-label-additional" }, additional)));
-
+        return (
+            <div className="pg-trading-header-progress-label">
+                <div className={className}>{progress} {bidUnit}</div>
+                <div className="pg-trading-header-progress-label-additional">{additional}</div>
+            </div>
+        );
     }
-
 }
 
 
+// WEBPACK FOOTER //
+// src/drone/src/src/containers/ToolBar/ProgressLabel/index.tsx
 
 
 
-
-
-
-
+// WEBPACK FOOTER //
+// ./src/containers/ToolBar/ProgressLabel/index.tsx
