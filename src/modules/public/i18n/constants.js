c837a2d49b127149b9d69eb40d5a55c7b74c91fd
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:26:32 +0100

    Update constants.js

--- src/modules/public/i18n/constants.js
+++ src/modules/public/i18n/constants.js
@@ -1,1 +1,10 @@
 export const CHANGE_LANGUAGE = 'i18n/CHANGE_LANGUAGE';
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/i18n/constants.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/i18n/constants.ts
