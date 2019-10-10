0adcf9393ffc0b25a3073b258c10a529e8b61960
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:19:40 +0100

    Update index.js

--- src/modules/user/kyc/identity/sagas/index.js
+++ src/modules/user/kyc/identity/sagas/index.js
@@ -1,11 +1,19 @@
+// tslint:disable-next-line
 import { takeEvery } from 'redux-saga/effects';
-
-import { SEND_IDENTITY_FETCH, } from '../constants';
-
+import {
+    SEND_IDENTITY_FETCH,
+} from '../constants';
 import { sendIdentitySaga } from './sendIdentitySaga';
 
 export function* rootSendIdentitySaga() {
-
     yield takeEvery(SEND_IDENTITY_FETCH, sendIdentitySaga);
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/kyc/identity/sagas/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/kyc/identity/sagas/index.ts
