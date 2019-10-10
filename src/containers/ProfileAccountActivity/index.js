94590cf57827c313dd60097d78f174c5b018b34f
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:44:23 +0100

    Update index.js

--- src/containers/ProfileAccountActivity/index.js
+++ src/containers/ProfileAccountActivity/index.js
@@ -1,203 +1,174 @@
-import { Pagination, Table } from '@openware/components';
-
 import classnames from 'classnames';
-
 import * as React from 'react';
+import {
+    FormattedMessage,
+    InjectedIntlProps,
+    injectIntl,
+} from 'react-intl';
+import { connect, MapDispatchToPropsFunction } from 'react-redux';
+import { localeDate } from '../../helpers';
+import {
+    getUserActivity,
+    RootState,
+    selectTotalNumber,
+    selectUserActivity,
+    selectUserActivityCurrentPage,
+    selectUserActivityFirstElemIndex,
+    selectUserActivityLastElemIndex,
+    selectUserActivityLoading,
+    selectUserActivityNextPageExists,
+    selectUserActivityPageCount,
+    UserActivityDataInterface,
+} from '../../modules';
+import { Pagination, Table } from '../../openware';
 
-import { FormattedMessage, injectIntl, } from 'react-intl';
+interface ReduxProps {
+    loading: boolean;
+    total: number;
+    page: number;
+    pageCount: number;
+    firstElemIndex: number;
+    lastElemIndex: number;
+    nextPageExists: boolean;
+    userActivity: UserActivityDataInterface[];
+}
 
-import { connect } from 'react-redux';
-
-import { localeFullDate } from '../../helpers';
-
-import { getUserActivity, selectTotalNumber, selectUserActivity, selectUserActivityCurrentPage, selectUserActivityFirstElemIndex, selectUserActivityLastElemIndex, selectUserActivityLoading, selectUserActivityNextPageExists, selectUserActivityPageCount, } from '../../modules';
+interface DispatchProps {
+    getUserActivity: typeof getUserActivity;
+}
 
 const paginationLimit = 25;
 
-class ProfileAccountActivityComponent extends React.Component {
+type Props = ReduxProps & DispatchProps & InjectedIntlProps;
 
-    constructor() {
-
-        super(...arguments);
-
-        this.renderContent = () => {
-
-            const { total, firstElemIndex, lastElemIndex, page, nextPageExists, userActivity } = this.props;
-
-            return (React.createElement(React.Fragment, null,
-
-                React.createElement(Table, { header: this.getHeaders(), data: this.getActivityData(userActivity) }),
-
-                React.createElement(Pagination, { firstElemIndex: firstElemIndex, lastElemIndex: lastElemIndex, total: total, page: page, nextPageExists: nextPageExists, onClickPrevPage: this.onClickPrevPage, onClickNextPage: this.onClickNextPage })));
-
-        };
-
-        this.getHeaders = () => {
-
-            return [
-
-                this.props.intl.formatMessage({ id: 'page.body.profile.header.accountActivity.content.date' }),
-
-                this.props.intl.formatMessage({ id: 'page.body.profile.header.accountActivity.content.action' }),
-
-                this.props.intl.formatMessage({ id: 'page.body.profile.header.accountActivity.content.result' }),
-
-                this.props.intl.formatMessage({ id: 'page.body.profile.header.accountActivity.content.addressip' }),
-
-                this.props.intl.formatMessage({ id: 'page.body.profile.header.accountActivity.content.userAgent' }),
-
-            ];
-
-        };
-
-        this.getResultOfUserAction = (value) => {
-
-            switch (value) {
-
-                case 'login':
-
-                    return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.login' });
-
-                case 'logout':
-
-                    return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.logout' });
-
-                case 'request QR code for 2FA':
-
-                    return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.request2fa' });
-
-                case 'enable 2FA':
-
-                    return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.enable2fa' });
-
-                case 'login::2fa':
-
-                    return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.login.2fa' });
-
-                case 'request password reset':
-
-                    return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.requestPasswordReset' });
-
-                case 'password reset':
-
-                    return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.passwordReset' });
-
-                default:
-
-                    return value;
-
-            }
-
-        };
-
-        this.onClickPrevPage = () => {
-
-            const { page } = this.props;
-
-            this.props.getUserActivity({ page: Number(page) - 1, limit: paginationLimit });
-
-        };
-
-        this.onClickNextPage = () => {
-
-            const { page } = this.props;
-
-            this.props.getUserActivity({ page: Number(page) + 1, limit: paginationLimit });
-
-        };
-
-    }
-
-    componentDidMount() {
-
+class ProfileAccountActivityComponent extends React.Component<Props> {
+    public componentDidMount() {
         this.props.getUserActivity({ page: 0, limit: paginationLimit });
-
     }
 
-    render() {
-
+    public render() {
         const { loading, userActivity } = this.props;
+        const emptyMsg = this.props.intl.formatMessage({id: 'page.noDataToShow'});
 
-        const emptyMsg = this.props.intl.formatMessage({ id: 'page.noDataToShow' });
-
-        return (React.createElement("div", { className: "pg-profile-page__activity" },
-
-            React.createElement("div", { className: "pg-profile-page-header" },
-
-                React.createElement("h3", null,
-
-                    React.createElement(FormattedMessage, { id: "page.body.profile.header.accountActivity" }))),
-
-            React.createElement("div", { className: `pg-history-elem ${userActivity.length ? '' : 'pg-history-empty'}` },
-
-                userActivity.length ? this.renderContent() : null,
-
-                !userActivity.length && !loading ? React.createElement("p", { className: "pg-history-elem__empty" }, emptyMsg) : null)));
-
+        return (
+            <div className="pg-profile-page__activity">
+                <div className="pg-profile-page-header">
+                    <h3><FormattedMessage id="page.body.profile.header.accountActivity" /></h3>
+                </div>
+                <div className={`pg-history-elem ${userActivity.length ? '' : 'pg-history-empty'}`}>
+                    {userActivity.length ? this.renderContent() : null}
+                    {!userActivity.length && !loading ? <p className="pg-history-elem__empty">{emptyMsg}</p> : null}
+                </div>
+            </div>
+        );
     }
 
-    getActivityData(userData) {
+    public renderContent = () => {
+        const { total, firstElemIndex, lastElemIndex, page, nextPageExists, userActivity } = this.props;
+        return (
+            <React.Fragment>
+                <Table
+                    header={this.getHeaders()}
+                    data={this.getActivityData(userActivity)}
+                />
+                <Pagination
+                    firstElemIndex={firstElemIndex}
+                    lastElemIndex={lastElemIndex}
+                    total={total}
+                    page={page}
+                    nextPageExists={nextPageExists}
+                    onClickPrevPage={this.onClickPrevPage}
+                    onClickNextPage={this.onClickNextPage}
+                />
+            </React.Fragment>
+        );
+    };
 
+    private getHeaders = () => {
+        return [
+            this.props.intl.formatMessage({id: 'page.body.profile.header.accountActivity.content.date'}),
+            this.props.intl.formatMessage({id: 'page.body.profile.header.accountActivity.content.action'}),
+            this.props.intl.formatMessage({id: 'page.body.profile.header.accountActivity.content.result'}),
+            this.props.intl.formatMessage({id: 'page.body.profile.header.accountActivity.content.addressip'}),
+            this.props.intl.formatMessage({id: 'page.body.profile.header.accountActivity.content.userAgent'}),
+        ];
+    };
+
+    private getActivityData(userData: UserActivityDataInterface[]) {
         return userData.map(item => {
-
             return [
-
-                localeFullDate(item.created_at),
-
+                localeDate(item.created_at, 'fullDate'),
                 this.getResultOfUserAction(item.action),
-
-                this.renderResult(this.props.intl.formatMessage({ id: `page.body.profile.content.result.${item.result}` })),
-
+                this.renderResult(this.props.intl.formatMessage({ id: `page.body.profile.content.result.${item.result}`})),
                 item.user_ip,
-
                 item.user_agent,
-
             ];
-
         });
-
     }
 
-    renderResult(result) {
-
+    private renderResult(result: string) {
         const className = classnames({
-
-            'pg-profile-page__activity-result-succeed': result === this.props.intl.formatMessage({ id: 'page.body.profile.content.result.succeed' }),
-
-            'pg-profile-page__activity-result-failed': result === this.props.intl.formatMessage({ id: 'page.body.profile.content.result.failed' }),
-
+            'pg-profile-page__activity-result-succeed': result === this.props.intl.formatMessage({id: 'page.body.profile.content.result.succeed'}),
+            'pg-profile-page__activity-result-failed': result === this.props.intl.formatMessage({id: 'page.body.profile.content.result.failed'}),
         });
-
-        return React.createElement("span", { className: className }, result);
-
+        return <span className={className}>{result}</span>;
     }
 
+    private getResultOfUserAction = (value: string) => {
+        switch (value) {
+            case 'login':
+                return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.login'});
+            case 'logout':
+                return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.logout'});
+            case 'request QR code for 2FA':
+                return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.request2fa'});
+            case 'enable 2FA':
+                return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.enable2fa'});
+            case 'login::2fa':
+                return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.login.2fa'});
+            case 'request password reset':
+                return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.requestPasswordReset'});
+            case 'password reset':
+                return this.props.intl.formatMessage({ id: 'page.body.profile.content.action.passwordReset'});
+            default:
+                return value;
+        }
+    }
+
+    private onClickPrevPage = () => {
+        const { page } = this.props;
+        this.props.getUserActivity({ page: Number(page) - 1, limit: paginationLimit });
+    };
+
+    private onClickNextPage = () => {
+        const { page } = this.props;
+        this.props.getUserActivity({ page: Number(page) + 1, limit: paginationLimit });
+    };
 }
 
-const mapStateToProps = (state) => ({
-
+const mapStateToProps = (state: RootState): ReduxProps => ({
     userActivity: selectUserActivity(state),
-
     loading: selectUserActivityLoading(state),
-
     total: selectTotalNumber(state),
-
     page: selectUserActivityCurrentPage(state),
-
     pageCount: selectUserActivityPageCount(state, paginationLimit),
-
     firstElemIndex: selectUserActivityFirstElemIndex(state, paginationLimit),
-
     lastElemIndex: selectUserActivityLastElemIndex(state, paginationLimit),
-
     nextPageExists: selectUserActivityNextPageExists(state, paginationLimit),
-
 });
 
-const mapDispatchToProps = dispatch => ({
-
-    getUserActivity: params => dispatch(getUserActivity(params)),
-
-});
+const mapDispatchToProps: MapDispatchToPropsFunction<DispatchProps, {}> =
+    dispatch => ({
+        getUserActivity: params => dispatch(getUserActivity(params)),
+    });
 
 export const ProfileAccountActivity = injectIntl(connect(mapStateToProps, mapDispatchToProps)(ProfileAccountActivityComponent));
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/containers/ProfileAccountActivity/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/containers/ProfileAccountActivity/index.tsx
