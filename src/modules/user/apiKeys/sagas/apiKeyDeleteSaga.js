0fc2aabce37d9d3fec263a1f67a1b9a81c8ab1a4
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:49:45 +0100

    Update apiKeyDeleteSaga.js

--- src/modules/user/apiKeys/sagas/apiKeyDeleteSaga.js
+++ src/modules/user/apiKeys/sagas/apiKeyDeleteSaga.js
@@ -1,41 +1,31 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../public/alert';
+import { apiKeyDelete, ApiKeyDeleteFetch, apiKeys2FAModal } from '../actions';
 
-import { apiKeyDelete, apiKeys2FAModal } from '../actions';
-
-const deleteOptions = {
-
+const deleteOptions: RequestOptions = {
     apiVersion: 'barong',
-
 };
 
-export function* apiKeyDeleteSaga(action) {
-
+export function* apiKeyDeleteSaga(action: ApiKeyDeleteFetch) {
     try {
-
-        const { kid, totp_code } = action.payload;
-
+        const {kid, totp_code} = action.payload;
         yield call(API.delete(deleteOptions), `/resource/api_keys/${kid}?totp_code=${totp_code}`);
-
-        yield put(apiKeyDelete({ kid }));
-
-        yield put(alertPush({ message: ['success.api_keys.deleted'], type: 'success' }));
-
+        yield put(apiKeyDelete({kid}));
+        yield put(alertPush({message: ['success.api_keys.deleted'], type: 'success'}));
+    } catch (error) {
+        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
+    } finally {
+        yield put(apiKeys2FAModal({active: false}));
     }
-
-    catch (error) {
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
-    }
-
-    finally {
-
-        yield put(apiKeys2FAModal({ active: false }));
-
-    }
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/apiKeys/sagas/apiKeyDeleteSaga.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/apiKeys/sagas/apiKeyDeleteSaga.ts
