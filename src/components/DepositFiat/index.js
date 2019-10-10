036f2039b47b8bcc12e32f133d7570b776b06a57
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:13:40 +0100

    Update index.js

--- src/components/DepositFiat/index.js
+++ src/components/DepositFiat/index.js
@@ -1,85 +1,77 @@
 import * as React from 'react';
-
 import { FormattedMessage } from 'react-intl';
 
+
+interface DepositFiatProps {
+    /**
+     * Sets helper description
+     */
+    description: string;
+    /**
+     * Sets title describing the data displayed in children
+     */
+    title: string;
+}
+
+
 const bankData = [
-
     {
-
-        key: React.createElement(FormattedMessage, { id: "page.body.wallets.tabs.deposit.fiat.bankName" }),
-
+        key: <FormattedMessage id="page.body.wallets.tabs.deposit.fiat.bankName" />,
         value: 'Diamant Bank',
-
     },
-
     {
-
-        key: React.createElement(FormattedMessage, { id: "page.body.wallets.tabs.deposit.fiat.accountNumber" }),
-
+        key: <FormattedMessage id="page.body.wallets.tabs.deposit.fiat.accountNumber" />,
         value: '10120212',
-
     },
-
     {
-
-        key: React.createElement(FormattedMessage, { id: "page.body.wallets.tabs.deposit.fiat.accountName" }),
-
+        key: <FormattedMessage id="page.body.wallets.tabs.deposit.fiat.accountName" />,
         value: 'name',
-
     },
-
     {
-
-        key: React.createElement(FormattedMessage, { id: "page.body.wallets.tabs.deposit.fiat.phoneNumber" }),
-
+        key: <FormattedMessage id="page.body.wallets.tabs.deposit.fiat.phoneNumber" />,
         value: '+3 8093 1212 12 12',
-
     },
-
     {
-
-        key: React.createElement(FormattedMessage, { id: "page.body.wallets.tabs.deposit.fiat.referenceCode" }),
-
+        key: <FormattedMessage id="page.body.wallets.tabs.deposit.fiat.referenceCode" />,
         value: '8374982374',
-
     },
-
 ];
 
 /**
-
  * Component to display bank account details which can be used for a
-
  * deposit
-
  */
-
-const DepositFiat = (props) => {
-
+const DepositFiat: React.FunctionComponent<DepositFiatProps> = (props: DepositFiatProps) => {
     const { description, title } = props;
 
-    const renderDetails = (detail, index) => {
-
-        return (React.createElement("div", { className: "cr-deposit-fiat-detail", key: index },
-
-            React.createElement("p", { className: "cr-deposit-fiat-detail__label" },
-
-                detail.key,
-
-                ":"),
-
-            React.createElement("p", { className: "cr-deposit-fiat-detail__value" }, detail.value)));
-
+    const renderDetails = (detail, index: number) => {
+        return (
+            <div className="cr-deposit-fiat-detail" key={index}>
+                <p className="cr-deposit-fiat-detail__label">{detail.key}:</p>
+                <p className="cr-deposit-fiat-detail__value">{detail.value}</p>
+            </div>
+        );
     };
 
-    return (React.createElement("div", { className: "cr-deposit-fiat" },
-
-        React.createElement("p", { className: "cr-deposit-fiat__title" }, title),
-
-        React.createElement("p", { className: "cr-deposit-fiat__description" }, description),
-
-        React.createElement("div", { className: "cr-deposit-fiat-credentials" }, bankData.map(renderDetails))));
-
+    return (
+        <div className="cr-deposit-fiat">
+            <p className="cr-deposit-fiat__title">{title}</p>
+            <p className="cr-deposit-fiat__description">{description}</p>
+            <div className="cr-deposit-fiat-credentials">{bankData.map(renderDetails)}</div>
+        </div>
+    );
 };
 
-export { DepositFiat, };
+export {
+    DepositFiat,
+    DepositFiatProps,
+};
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/components/DepositFiat/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/components/DepositFiat/index.tsx
