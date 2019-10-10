d61c864ad19817d1a868b5c94909fd6a5f4aced7
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:07:59 +0100

    Update reducer.js

--- src/modules/user/emailVerification/reducer.js
+++ src/modules/user/emailVerification/reducer.js
@@ -1,59 +1,55 @@
-import { EMAIL_VERIFICATION_DATA, EMAIL_VERIFICATION_ERROR, EMAIL_VERIFICATION_FETCH, } from './constants';
+import {
+    CommonError,
+} from '../../types';
+import { EmailVerificationAction } from './actions';
+import {
+    EMAIL_VERIFICATION_DATA,
+    EMAIL_VERIFICATION_ERROR,
+    EMAIL_VERIFICATION_FETCH,
+} from './constants';
 
-export const initialState = {
+export interface EmailVerificationState {
+    loading: boolean;
+    success: boolean;
+    error?: CommonError;
+}
 
+export const initialState: EmailVerificationState = {
     loading: false,
-
     success: false,
-
 };
 
-export const sendEmailVerificationReducer = (state = initialState, action) => {
-
+export const sendEmailVerificationReducer = (state = initialState, action: EmailVerificationAction) => {
     switch (action.type) {
-
         case EMAIL_VERIFICATION_DATA:
-
             return {
-
                 ...state,
-
                 loading: false,
-
                 success: true,
-
             };
-
         case EMAIL_VERIFICATION_ERROR:
-
             return {
-
                 ...state,
-
                 loading: false,
-
                 success: false,
-
                 error: action.error,
-
             };
-
         case EMAIL_VERIFICATION_FETCH:
-
             return {
-
                 ...state,
-
                 loading: true,
-
                 success: false,
-
             };
-
         default:
-
             return state;
-
     }
-
 };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/emailVerification/reducer.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/emailVerification/reducer.ts
