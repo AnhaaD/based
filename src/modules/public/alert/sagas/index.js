4047b913c50ecddc2a9ed0cba841ed0807e474f1
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:12:48 +0100

    Update index.js

--- src/modules/public/alert/sagas/index.js
+++ src/modules/public/alert/sagas/index.js
@@ -1,11 +1,17 @@
+// tslint:disable-next-line
 import { takeEvery } from 'redux-saga/effects';
-
 import { ALERT_PUSH } from '../constants';
-
 import { handleAlertSaga } from './handleAlertSaga';
 
 export function* rootHandleAlertSaga() {
-
     yield takeEvery(ALERT_PUSH, handleAlertSaga);
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/alert/sagas/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/alert/sagas/index.ts
