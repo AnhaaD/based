2ab5f3c10a8370fd4e365fae0fe28e0577c152f9
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:15:17 +0100

    Update index.js

--- src/modules/user/kyc/documents/sagas/index.js
+++ src/modules/user/kyc/documents/sagas/index.js
@@ -1,11 +1,19 @@
+// tslint:disable-next-line
 import { takeEvery } from 'redux-saga/effects';
-
-import { SEND_DOCUMENTS_FETCH, } from '../constants';
-
+import {
+    SEND_DOCUMENTS_FETCH,
+} from '../constants';
 import { sendDocumentsSaga } from './sendDocumentsSaga';
 
 export function* rootSendDocumentsSaga() {
-
     yield takeEvery(SEND_DOCUMENTS_FETCH, sendDocumentsSaga);
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/kyc/documents/sagas/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/kyc/documents/sagas/index.ts
