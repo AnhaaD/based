2c222926e0730ec55df39bc63513bd0f02adc705
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:14:00 +0100

    Update reducer.js

--- src/modules/user/history/reducer.js
+++ src/modules/user/history/reducer.js
@@ -1,63 +1,59 @@
-import { HISTORY_DATA, HISTORY_ERROR, HISTORY_FETCH, HISTORY_PUSH_FINISH, HISTORY_RESET, } from './constants';
+import { HistoryActions } from './actions';
+import {
+    HISTORY_DATA,
+    HISTORY_ERROR,
+    HISTORY_FETCH,
+    HISTORY_PUSH_FINISH,
+    HISTORY_RESET,
+} from './constants';
+import { WalletHistoryList } from './types';
 
-const initialState = {
+export interface HistoryState {
+    list: WalletHistoryList;
+    fetching: boolean;
+    fullHistory: number;
+    page: number;
+}
 
+const initialState: HistoryState = {
     list: [],
-
     fetching: false,
-
     fullHistory: 0,
-
     page: 0,
-
 };
 
-export const historyReducer = (state = initialState, action) => {
 
+export const historyReducer = (state = initialState, action: HistoryActions) => {
     switch (action.type) {
-
         case HISTORY_FETCH:
-
             return { ...state, fetching: true };
-
         case HISTORY_DATA:
-
             return {
-
                 ...state,
-
                 list: action.payload.list,
-
                 fetching: false,
-
                 page: action.payload.page,
-
                 fullHistory: action.payload.fullHistory,
-
             };
-
         case HISTORY_ERROR: {
-
             return { ...state, list: [], fetching: false, fullHistory: 0, page: 0 };
-
         }
-
         case HISTORY_RESET: {
-
             return { ...state, list: [], fullHistory: 0, page: 0 };
-
         }
-
         case HISTORY_PUSH_FINISH: {
-
             return { ...state, list: action.payload };
-
         }
-
         default:
-
             return state;
-
     }
-
 };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/history/reducer.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/history/reducer.ts
