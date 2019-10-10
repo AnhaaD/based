689a45e6b93d1c8dea41a8148e263856344e241a
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:02:13 +0100

    Update constants.js

--- src/modules/user/emailVerification/constants.js
+++ src/modules/user/emailVerification/constants.js
@@ -1,5 +1,12 @@
 export const EMAIL_VERIFICATION_DATA = 'emailVerification/DATA';
-
 export const EMAIL_VERIFICATION_ERROR = 'emailVerification/ERROR';
-
 export const EMAIL_VERIFICATION_FETCH = 'emailVerification/FETCH';
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/emailVerification/constants.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/emailVerification/constants.ts
