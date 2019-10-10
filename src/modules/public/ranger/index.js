d6802d91e1a01bd454ff347fe7fd1f513d4aba4f
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:42:08 +0100

    Update index.js

--- src/modules/public/ranger/index.js
+++ src/modules/public/ranger/index.js
@@ -1,3 +1,13 @@
 export * from './sagas';
-
 export * from './actions';
+export * from './reducer';
+export * from './selectors';
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/ranger/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/ranger/index.ts
