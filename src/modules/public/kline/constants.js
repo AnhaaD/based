5d1bc4c7137bd8a7a8a34368390a1b39f5251a53
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:29:42 +0100

    Update constants.js

--- src/modules/public/kline/constants.js
+++ src/modules/public/kline/constants.js
@@ -1,5 +1,16 @@
 export const KLINE_PUSH = 'kline/PUSH';
 
 export const KLINE_FETCH = 'kline/KLINE_FETCH';
-
 export const KLINE_DATA = 'kline/KLINE_DATA';
+
+export const KLINE_UPDATE_TIME_RANGE = 'kline/KLINE_UPDATE_TIME_RANGE';
+export const KLINE_UPDATE_PERIOD = 'kline/KLINE_UPDATE_PERIOD';
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/kline/constants.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/kline/constants.ts
