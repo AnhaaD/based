1b3b33fa28e4ada6b400030513f65fa427dee590
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:45:07 +0100

    Update actions.js

--- src/modules/public/recentTrades/actions.js
+++ src/modules/public/recentTrades/actions.js
@@ -1,33 +1,69 @@
-import { RECENT_TRADES_DATA, RECENT_TRADES_ERROR, RECENT_TRADES_FETCH, RECENT_TRADES_PUSH, } from './constants';
+import { CommonError } from '../../types';
+import {
+    RECENT_TRADES_DATA,
+    RECENT_TRADES_ERROR,
+    RECENT_TRADES_FETCH,
+    RECENT_TRADES_PUSH,
+} from './constants';
 
-export const recentTradesFetch = (payload) => ({
+import { PublicTrade } from '../../user/history';
+import { Market } from '../markets';
+import { PublicTradeEvent } from './types';
 
+export interface RecentTradesFetch {
+    type: typeof RECENT_TRADES_FETCH;
+    payload: Market;
+}
+
+export interface RecentTradesData {
+    type: typeof RECENT_TRADES_DATA;
+    payload: PublicTrade[];
+}
+
+export interface RecentTradesError {
+    type: typeof RECENT_TRADES_ERROR;
+    payload?: CommonError;
+}
+
+export interface RecentTradesPush {
+    type: typeof RECENT_TRADES_PUSH;
+    payload: {
+        trades: PublicTradeEvent[],
+        market: string,
+    };
+}
+
+export type RecentTradesActions =
+    RecentTradesFetch
+    | RecentTradesError
+    | RecentTradesData
+    | RecentTradesPush;
+
+export const recentTradesFetch = (payload: RecentTradesFetch['payload']): RecentTradesFetch => ({
     type: RECENT_TRADES_FETCH,
-
     payload,
-
 });
 
-export const recentTradesData = (payload) => ({
-
+export const recentTradesData = (payload: RecentTradesData['payload']): RecentTradesData => ({
     type: RECENT_TRADES_DATA,
-
     payload,
-
 });
 
-export const recentTradesPush = (payload) => ({
-
+export const recentTradesPush = (payload: RecentTradesPush['payload']): RecentTradesPush => ({
     type: RECENT_TRADES_PUSH,
-
     payload,
-
 });
 
-export const recentTradesError = (payload) => ({
-
+export const recentTradesError = (payload: RecentTradesError['payload']): RecentTradesError => ({
     type: RECENT_TRADES_ERROR,
-
     payload,
-
 });
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/recentTrades/actions.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/recentTrades/actions.ts
