ceccb3e3dae8c08130d8dfa0a9dc3e4020d16d1c
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:26:56 +0100

    Update index.js

--- src/modules/public/i18n/index.js
+++ src/modules/public/i18n/index.js
@@ -1,5 +1,12 @@
 export * from './actions';
-
 export * from './reducer';
-
 export * from './selectors';
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/i18n/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/i18n/index.ts
