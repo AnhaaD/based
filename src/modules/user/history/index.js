b1f2467b28888f5005b2fbda93fac1ca6ad7a2d0
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:13:28 +0100

    Update index.js

--- src/modules/user/history/index.js
+++ src/modules/user/history/index.js
@@ -1,9 +1,14 @@
 export * from './reducer';
-
 export * from './actions';
-
 export * from './selectors';
-
 export * from './sagas/index';
+export * from './types';
 
-// export * from './types';
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/history/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/history/index.ts
