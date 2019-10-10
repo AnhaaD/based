e9b490001e6f118dd6e4e680de1e8cea6283086b
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:35:55 +0100

    Update index.js

--- src/modules/public/orderBook/sagas/index.js
+++ src/modules/public/orderBook/sagas/index.js
@@ -1,15 +1,22 @@
+// tslint:disable-next-line
 import { takeLatest } from 'redux-saga/effects';
-
-import { DEPTH_FETCH, ORDER_BOOK_FETCH, } from '../constants';
-
+import {
+    DEPTH_FETCH,
+    ORDER_BOOK_FETCH,
+} from '../constants';
 import { depthSaga } from './depthSaga';
-
 import { orderBookSaga } from './orderBookSaga';
 
 export function* rootOrderBookSaga() {
-
     yield takeLatest(ORDER_BOOK_FETCH, orderBookSaga);
-
     yield takeLatest(DEPTH_FETCH, depthSaga);
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/orderBook/sagas/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/orderBook/sagas/index.ts
