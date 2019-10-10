365f158821ef6d43356087b1c1a5c662268e7512
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:28:47 +0100

    Update index.js

--- src/modules/public/kline/sagas/index.js
+++ src/modules/public/kline/sagas/index.js
@@ -1,11 +1,17 @@
+// tslint:disable-next-line
 import { takeEvery } from 'redux-saga/effects';
-
 import { KLINE_FETCH } from '../constants';
-
 import { handleKlineFetchSaga } from './handleKlineFetchSaga';
 
 export function* rootKlineFetchSaga() {
-
     yield takeEvery(KLINE_FETCH, handleKlineFetchSaga);
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/kline/sagas/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/kline/sagas/index.ts
