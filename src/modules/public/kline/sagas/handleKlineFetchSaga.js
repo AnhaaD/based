a6f4d4386928c14c5efa222d148dd19e772770a9
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:28:18 +0100

    Update handleKlineFetchSaga.js

--- src/modules/public/kline/sagas/handleKlineFetchSaga.js
+++ src/modules/public/kline/sagas/handleKlineFetchSaga.js
@@ -1,75 +1,55 @@
-
-
+// tslint:disable-next-line no-submodule-imports
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../alert';
+import { klineData, KlineFetch } from '../actions';
 
-import { klineData } from '../actions';
-
-const klineRequestOptions = {
-
+const klineRequestOptions: RequestOptions = {
     apiVersion: 'peatio',
-
 };
 
-export function* handleKlineFetchSaga(action) {
+export function* handleKlineFetchSaga(action: KlineFetch) {
+  try {
+      const {
+          market,
+          resolution,
+          from,
+          to,
+      } = action.payload;
+      const data = yield call(API.get(klineRequestOptions), `/public/markets/${market}/k-line?period=${resolution}&time_from=${from}&time_to=${to}`);
 
-    try {
+      const convertedData = data.map(elem => {
+          const [date, open, high, low, close, volume] = elem.map(e => {
+              switch (typeof e) {
+                  case 'number':
+                      return e;
+                  case 'string':
+                      return Number.parseFloat(e);
+                  default:
+                      throw (new Error(`unexpected type ${typeof e}`));
+              }
+          });
 
-        const { market, resolution, from, to, } = action.payload;
-
-        const data = yield call(API.get(klineRequestOptions), `/public/markets/${market}/k-line?period=${resolution}&time_from=${from}&time_to=${to}`);
-
-        const convertedData = data.map(elem => {
-
-            const [date, open, high, low, close, volume] = elem.map(e => {
-
-                switch (typeof e) {
-
-                    case 'number':
-
-                        return e;
-
-                    case 'string':
-
-                        return Number.parseFloat(e);
-
-                    default:
-
-                        throw (new Error(`unexpected type ${typeof e}`));
-
-                }
-
-            });
-
-            return {
-
-                date: date * 1e3,
-
-                open,
-
-                high,
-
-                low,
-
-                close,
-
-                volume,
-
-            };
-
-        });
-
-        yield put(klineData(convertedData));
-
-    }
-
-    catch (error) {
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
-    }
-
+          return {
+              date: date * 1e3,
+              open,
+              high,
+              low,
+              close,
+              volume,
+          };
+      });
+      yield put(klineData(convertedData));
+  } catch (error) {
+      yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
+  }
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/kline/sagas/handleKlineFetchSaga.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/kline/sagas/handleKlineFetchSaga.ts
