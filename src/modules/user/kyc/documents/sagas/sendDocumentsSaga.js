b943312eefe02a665c1b479e6333263aa818e408
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:15:58 +0100

    Update sendDocumentsSaga.js

--- src/modules/user/kyc/documents/sagas/sendDocumentsSaga.js
+++ src/modules/user/kyc/documents/sagas/sendDocumentsSaga.js
@@ -1,39 +1,31 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../../api';
-
+import { API, RequestOptions } from '../../../../../api';
 import { alertPush } from '../../../../public/alert';
+import { sendDocumentsData, sendDocumentsError, SendDocumentsFetch } from '../actions';
 
-import { sendDocumentsData, sendDocumentsError } from '../actions';
-
-const sessionsConfig = {
-
+const sessionsConfig: RequestOptions = {
     apiVersion: 'barong',
-
 };
 
-export function* sendDocumentsSaga(action) {
-
+export function* sendDocumentsSaga(action: SendDocumentsFetch) {
     try {
-
         const response = yield call(API.post(sessionsConfig), '/resource/documents', action.payload);
-
         const defaultMessage = 'success.documents.accepted';
-
         const { message = defaultMessage } = response;
-
         yield put(sendDocumentsData({ message }));
-
-        yield put(alertPush({ message: [defaultMessage], type: 'success' }));
-
-    }
-
-    catch (error) {
-
+        yield put(alertPush({ message: [defaultMessage], type: 'success'}));
+    } catch (error) {
         yield put(sendDocumentsError(error));
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
+        yield put(alertPush({ message: error.message, code: error.code, type: 'error'}));
     }
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/kyc/documents/sagas/sendDocumentsSaga.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/kyc/documents/sagas/sendDocumentsSaga.ts
