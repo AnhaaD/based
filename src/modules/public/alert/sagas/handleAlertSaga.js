0538c45c6398386c6c7a78872ed3af2657490004
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:12:21 +0100

    Update handleAlertSaga.js

--- src/modules/public/alert/sagas/handleAlertSaga.js
+++ src/modules/public/alert/sagas/handleAlertSaga.js
@@ -1,91 +1,50 @@
-import { delay } from 'redux-saga';
-
-
-
-import { call, put } from 'redux-saga/effects';
-
+// tslint:disable-next-line no-submodule-imports
+import { call, delay, put } from 'redux-saga/effects';
 import { userReset } from '../../../';
-
 import { msAlertDisplayTime } from '../../../../api';
+import { alertData, alertDelete, AlertPush } from '../actions';
 
-import { alertData, alertDelete } from '../actions';
-
-export function* handleAlertSaga(action) {
-
+export function* handleAlertSaga(action: AlertPush) {
     if (action.payload.type === 'error') {
-
         switch (action.payload.code) {
-
             case 401:
-
                 if (action.payload.message.indexOf('identity.session.not_active') > -1) {
-
                     yield put(userReset());
-
                     yield put(alertData(action.payload));
-
                     return;
-
-                }
-
-                else {
-
+                } else {
                     if (action.payload.message.indexOf('authz.invalid_session') > -1) {
-
                         yield put(userReset());
-
-                    }
-
-                    else {
-
+                    } else {
                         yield call(callAlertData, action);
-
                         break;
-
                     }
-
                 }
-
                 break;
-
             case 403:
-
                 if (action.payload.message.indexOf('identity.session.invalid_otp') > -1) {
-
                     yield call(callAlertData, action);
-
                 }
-
-                if (action.payload.message.indexOf('jwt.decode_and_verify') > -1) {
-
-                    yield call(callAlertData, action);
-
-                }
-
                 return;
-
             default:
-
                 yield call(callAlertData, action);
-
         }
-
-    }
-
-    else {
-
+    } else {
         yield call(callAlertData, action);
-
     }
-
 }
 
-function* callAlertData(action) {
-
+function* callAlertData(action: AlertPush) {
     yield put(alertData(action.payload));
-
     yield delay(parseFloat(msAlertDisplayTime()));
-
     yield put(alertDelete());
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/alert/sagas/handleAlertSaga.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/alert/sagas/handleAlertSaga.ts
