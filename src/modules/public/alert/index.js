0484456ca1a5781fb2e324c9816d811d114dcad9
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:14:56 +0100

    Update index.js

--- src/modules/public/alert/index.js
+++ src/modules/public/alert/index.js
@@ -1,7 +1,13 @@
 export * from './actions';
-
 export * from './sagas';
-
 export * from './reducer';
-
 export * from './selectors';
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/alert/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/alert/index.ts
