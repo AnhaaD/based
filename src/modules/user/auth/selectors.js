ec7421ac358ed528b3e45624b525695adc55a470
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:58:53 +0100

    Update selectors.js

--- src/modules/user/auth/selectors.js
+++ src/modules/user/auth/selectors.js
@@ -1,5 +1,20 @@
-export const selectSignInRequire2FA = (state) => state.user.auth.require2FA;
+import { RootState } from '../../index';
+import { AuthState } from './reducer';
 
-export const selectSignUpRequireVerification = (state) => state.user.auth.requireVerification;
+export const selectSignInRequire2FA = (state: RootState): AuthState['require2FA'] =>
+    state.user.auth.require2FA;
 
-export const selectEmailVerified = (state) => state.user.auth.emailVerified;
+export const selectSignUpRequireVerification = (state: RootState): AuthState['requireVerification'] =>
+    state.user.auth.requireVerification;
+
+export const selectEmailVerified = (state: RootState): AuthState['emailVerified'] =>
+    state.user.auth.emailVerified;
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/auth/selectors.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/auth/selectors.ts
