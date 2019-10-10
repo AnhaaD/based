d9acd0f5aaa73120e4b328ff44718786fe4ddf50
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:48:43 +0100

    Update apiKeyCreateSaga.js

--- src/modules/user/apiKeys/sagas/apiKeyCreateSaga.js
+++ src/modules/user/apiKeys/sagas/apiKeyCreateSaga.js
@@ -1,37 +1,30 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../public/alert';
+import { apiKeyCreate, ApiKeyCreateFetch, apiKeys2FAModal } from '../actions';
 
-import { apiKeyCreate, apiKeys2FAModal } from '../actions';
-
-const createOptions = {
-
+const createOptions: RequestOptions = {
     apiVersion: 'barong',
-
 };
 
-export function* apiKeyCreateSaga(action) {
-
+export function* apiKeyCreateSaga(action: ApiKeyCreateFetch) {
     try {
-
         const apiKey = yield call(API.post(createOptions), '/resource/api_keys', action.payload);
-
         yield put(apiKeyCreate(apiKey));
-
-        yield put(alertPush({ message: ['success.api_keys.created'], type: 'success' }));
-
-        yield put(apiKeys2FAModal({ active: true, action: 'createSuccess', apiKey }));
-
+        yield put(alertPush({message: ['success.api_keys.created'], type: 'success'}));
+        yield put(apiKeys2FAModal({active: true, action: 'createSuccess', apiKey}));
+    } catch (error) {
+        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
+        yield put(apiKeys2FAModal({active: false}));
     }
-
-    catch (error) {
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
-        yield put(apiKeys2FAModal({ active: false }));
-
-    }
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/apiKeys/sagas/apiKeyCreateSaga.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/apiKeys/sagas/apiKeyCreateSaga.ts
