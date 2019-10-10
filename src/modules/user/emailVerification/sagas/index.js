c673d3bdc288b8621e67f7472d61d06b8b0b2e37
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:00:57 +0100

    Update index.js

--- src/modules/user/emailVerification/sagas/index.js
+++ src/modules/user/emailVerification/sagas/index.js
@@ -1,11 +1,19 @@
+// tslint:disable-next-line
 import { takeLatest } from 'redux-saga/effects';
-
-import { EMAIL_VERIFICATION_FETCH, } from '../constants';
-
+import {
+    EMAIL_VERIFICATION_FETCH,
+} from '../constants';
 import { emailVerificationSaga } from './emailVerificationSaga';
 
 export function* rootEmailVerificationSaga() {
-
     yield takeLatest(EMAIL_VERIFICATION_FETCH, emailVerificationSaga);
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/emailVerification/sagas/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/emailVerification/sagas/index.ts
