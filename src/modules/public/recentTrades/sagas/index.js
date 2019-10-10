f3fc2361719018c33f92bef029dab366d0dc75ca
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:44:12 +0100

    Update index.js

--- src/modules/public/recentTrades/sagas/index.js
+++ src/modules/public/recentTrades/sagas/index.js
@@ -1,11 +1,17 @@
+// tslint:disable-next-line
 import { takeEvery } from 'redux-saga/effects';
-
 import { RECENT_TRADES_FETCH } from '../constants';
-
 import { recentTradesFetchSaga } from './recentTradesFetchSaga';
 
 export function* rootRecentTradesSaga() {
-
     yield takeEvery(RECENT_TRADES_FETCH, recentTradesFetchSaga);
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/recentTrades/sagas/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/recentTrades/sagas/index.ts
