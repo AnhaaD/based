1626f6ba061caedfa9e76fed3f34306503852e89
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:48:33 +0100

    Update index.js

--- src/containers/RecentTrades/index.js
+++ src/containers/RecentTrades/index.js
@@ -1,149 +1,124 @@
-import { Decimal, Table } from '@openware/components';
-
 import classnames from 'classnames';
-
 import * as React from 'react';
-
-import { FormattedMessage, injectIntl, } from 'react-intl';
-
-import { connect } from 'react-redux';
-
-import { localeDateSec, setTradeColor } from '../../helpers';
-
-import { selectCurrentMarket, selectCurrentPrice, setCurrentPrice, } from '../../modules';
-
+import {
+    FormattedMessage,
+    InjectedIntlProps,
+    injectIntl,
+} from 'react-intl';
+import { connect, MapDispatchToPropsFunction } from 'react-redux';
+import { localeDate, setTradeColor } from '../../helpers';
+import {
+    Market,
+    PublicTrade,
+    RootState,
+    selectCurrentMarket,
+    selectCurrentPrice,
+    setCurrentPrice,
+} from '../../modules';
 import { recentTradesFetch, selectRecentTradesOfCurrentMarket } from '../../modules/public/recentTrades';
+import { Decimal, Table } from '../../openware';
 
-class RecentTradesComponent extends React.Component {
+interface ReduxProps {
+    recentTrades: PublicTrade[];
+    currentMarket: Market | undefined;
+    currentPrice: string;
+}
 
-    constructor() {
+interface DispatchProps {
+    tradesFetch: typeof recentTradesFetch;
+    setCurrentPrice: typeof setCurrentPrice;
+}
 
-        super(...arguments);
+type Props = DispatchProps & ReduxProps & InjectedIntlProps;
 
-        this.getHeaders = () => {
-
-            return [
-
-                this.props.intl.formatMessage({ id: 'page.body.trade.header.recentTrades.content.price' }),
-
-                this.props.intl.formatMessage({ id: 'page.body.trade.header.recentTrades.content.amount' }),
-
-                this.props.intl.formatMessage({ id: 'page.body.trade.header.recentTrades.content.time' }),
-
-            ];
-
-        };
-
-        this.handleOnSelect = (index) => {
-
-            const { recentTrades, currentPrice } = this.props;
-
-            const priceToSet = recentTrades[Number(index)] ? recentTrades[Number(index)].price : '';
-
-            if (currentPrice !== priceToSet) {
-
-                this.props.setCurrentPrice(priceToSet);
-
-            }
-
-        };
-
-    }
-
-    componentWillReceiveProps(next) {
-
+class RecentTradesComponent extends React.Component<Props> {
+    public componentWillReceiveProps(next: Props) {
         if (next.currentMarket && this.props.currentMarket !== next.currentMarket) {
-
-            this.props.tradesFetch(next.currentMarket);
-
+          this.props.tradesFetch(next.currentMarket);
         }
-
     }
 
-    componentDidMount() {
-
-        if (this.props.currentMarket) {
-
-            this.props.tradesFetch(this.props.currentMarket);
-
+    public componentDidMount() {
+        if (this.props.currentMarket){
+          this.props.tradesFetch(this.props.currentMarket);
         }
-
     }
 
-    render() {
+    public render() {
 
         const className = classnames({
-
-            'cr-table__noData': !this.props.recentTrades.length,
-
+            'cr-table__noData' : !this.props.recentTrades.length,
         });
-
-        return (React.createElement("div", { className: className },
-
-            React.createElement("div", { className: "pg-recent-trades" },
-
-                React.createElement("div", { className: "cr-table-header__content" },
-
-                    React.createElement("div", { className: "cr-title-component" },
-
-                        React.createElement(FormattedMessage, { id: "page.body.trade.header.recentTrades" }))),
-
-                React.createElement(Table, { data: this.getTrades(this.props.recentTrades), header: this.getHeaders(), onSelect: this.handleOnSelect }))));
-
+        return (
+          <div className={className}>
+            <div className="pg-recent-trades">
+                <div className="cr-table-header__content">
+                    <div className="cr-title-component"><FormattedMessage id="page.body.trade.header.recentTrades" /></div>
+                </div>
+                <Table
+                    data={this.getTrades(this.props.recentTrades)}
+                    header={this.getHeaders()}
+                    onSelect={this.handleOnSelect}
+                />
+            </div>
+          </div>
+        );
     }
 
-    getTrades(trades) {
+    private getHeaders = () => {
+        return [
+            this.props.intl.formatMessage({ id: 'page.body.trade.header.recentTrades.content.price'}),
+            this.props.intl.formatMessage({ id: 'page.body.trade.header.recentTrades.content.amount'}),
+            this.props.intl.formatMessage({ id: 'page.body.trade.header.recentTrades.content.time'}),
+        ];
+    };
 
+    private getTrades(trades: PublicTrade[]) {
         const priceFixed = this.props.currentMarket ? this.props.currentMarket.bid_precision : 0;
-
         const amountFixed = this.props.currentMarket ? this.props.currentMarket.ask_precision : 0;
 
         const renderRow = item => {
-
             const { id, created_at, taker_type, price, volume } = item;
-
             return [
-
-                React.createElement("span", { style: { color: setTradeColor(taker_type).color }, key: id },
-
-                    React.createElement(Decimal, { fixed: priceFixed }, price)),
-
-                React.createElement("span", { style: { color: setTradeColor(taker_type).color }, key: id },
-
-                    React.createElement(Decimal, { fixed: amountFixed }, volume)),
-
-                React.createElement("span", { style: { color: setTradeColor(taker_type).color }, key: id }, localeDateSec(created_at).slice(5)),
-
+                <span style={{ color: setTradeColor(taker_type).color }} key={id}><Decimal fixed={priceFixed}>{price}</Decimal></span>,
+                <span style={{ color: setTradeColor(taker_type).color }} key={id}><Decimal fixed={amountFixed}>{volume}</Decimal></span>,
+                <span style={{ color: setTradeColor(taker_type).color }} key={id}>{localeDate(created_at, 'time')}</span>,
             ];
-
         };
-
         return (trades.length > 0)
-
             ? trades.map(renderRow)
-
-            : [[[''], this.props.intl.formatMessage({ id: 'page.noDataToShow' })]];
-
+            : [[[''], this.props.intl.formatMessage({ id: 'page.noDataToShow'})]];
     }
 
+    private handleOnSelect = (index: string) => {
+        const { recentTrades, currentPrice } = this.props;
+        const priceToSet = recentTrades[Number(index)] ? recentTrades[Number(index)].price : '';
+
+        if (currentPrice !== priceToSet) {
+            this.props.setCurrentPrice(priceToSet);
+        }
+    };
 }
 
-const mapStateToProps = (state) => ({
-
+const mapStateToProps = (state: RootState): ReduxProps => ({
     recentTrades: selectRecentTradesOfCurrentMarket(state),
-
     currentMarket: selectCurrentMarket(state),
-
     currentPrice: selectCurrentPrice(state),
-
 });
 
-const mapDispatchToProps = dispatch => ({
-
+const mapDispatchToProps: MapDispatchToPropsFunction<DispatchProps, {}> = dispatch => ({
     tradesFetch: market => dispatch(recentTradesFetch(market)),
-
     setCurrentPrice: payload => dispatch(setCurrentPrice(payload)),
-
 });
+
 
 export const RecentTrades = injectIntl(connect(mapStateToProps, mapDispatchToProps)(RecentTradesComponent));
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/containers/RecentTrades/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/containers/RecentTrades/index.tsx
