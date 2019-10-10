591addeeae5001d8a1b317a37e570484c30ba08f
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:24:15 +0100

    Create constants.js

---
+++ src/modules/public/gridLayout/constants.js
@@ -0,0 +1,11 @@
+export const SAVE_LAYOUTS = 'layouts/SAVE_LAYOUTS';
+export const RESET_LAYOUTS = 'layouts/RESET_LAYOUTS';
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/gridLayout/constants.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/gridLayout/constants.ts
