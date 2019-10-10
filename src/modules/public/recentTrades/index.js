02b652b25258a297a6db1b143a973dd1885746ab
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:45:58 +0100

    Update index.js

--- src/modules/public/recentTrades/index.js
+++ src/modules/public/recentTrades/index.js
@@ -1,9 +1,14 @@
 export * from './sagas';
-
 export * from './actions';
-
 export * from './reducer';
-
 export * from './selectors';
+export * from './types';
 
-// export * from './types';
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/recentTrades/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/recentTrades/index.ts
