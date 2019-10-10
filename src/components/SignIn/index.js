7d377eccb3d91ce628f8c7db057f716be4663f60
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:20:49 +0100

    Update index.js

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,149 +1,200 @@
-import { Button, } from '@openware/components';
-
 import cr from 'classnames';
-
 import * as React from 'react';
-
-import { CustomInput, } from '../';
-
-import { EMAIL_REGEX, } from '../../helpers';
-
-class SignInComponent extends React.Component {
-
-    constructor() {
-
-        super(...arguments);
-
-        this.handleChangeEmail = (value) => {
-
-            this.props.changeEmail(value);
-
-        };
-
-        this.handleChangePassword = (value) => {
-
-            this.props.changePassword(value);
-
-        };
-
-        this.handleFieldFocus = (field) => {
-
-            this.props.handleChangeFocusField(field);
-
-        };
-
-        this.handleSubmitForm = () => {
-
-            this.props.refreshError();
-
-            this.props.onSignIn();
-
-        };
-
-        this.isValidForm = () => {
-
-            const { email, password } = this.props;
-
-            const isEmailValid = email.match(EMAIL_REGEX);
-
-            return email && isEmailValid && password;
-
-        };
-
-        this.handleValidateForm = () => {
-
-            this.props.isFormValid();
-
-        };
-
-        this.handleClick = (label, e) => {
-
-            if (e) {
-
-                e.preventDefault();
-
-            }
-
-            if (!this.isValidForm()) {
-
-                this.handleValidateForm();
-
-            }
-
-            else {
-
-                this.handleSubmitForm();
-
-            }
-
-        };
-
-    }
-
-    render() {
-
-        const { email, emailError, emailPlaceholder, password, passwordError, passwordPlaceholder, isLoading, onForgotPassword, onSignUp, image, labelSignIn, labelSignUp, emailLabel, passwordLabel, forgotPasswordLabel, emailFocused, passwordFocused, } = this.props;
-
-        const emailGroupClass = cr('cr-sign-in-form__group', {
-
-            'cr-sign-in-form__group--focused': emailFocused,
-
-        });
-
-        const passwordGroupClass = cr('cr-sign-in-form__group', {
-
-            'cr-sign-in-form__group--focused': passwordFocused,
-
-        });
-
-        const logo = image ? (React.createElement("h1", { className: "cr-sign-in-form__title" },
-
-            React.createElement("img", { className: "cr-sign-in-form__image", src: image, alt: "logo" }))) : null;
-
-        
-
-        return (React.createElement("form", null,
-
-            React.createElement("div", { className: "cr-sign-in-form" },
-
-                React.createElement("div", { className: "cr-sign-in-form__options-group" },
-
-                    React.createElement("div", { className: "cr-sign-in-form__option" },
-
-                        React.createElement("div", { className: "cr-sign-in-form__option-inner __selected" }, labelSignIn ? labelSignIn : 'Sign In')),
-
-                    React.createElement("div", { className: "cr-sign-in-form__option" },
-
-                        React.createElement("div", { className: "cr-sign-in-form__option-inner cr-sign-in-form__tab-signup", onClick: onSignUp }, labelSignUp ? labelSignUp : 'Sign Up'))),
-
-                React.createElement("div", { className: "cr-sign-in-form__form-content" },
-
-                    logo,
-
-                    React.createElement("div", { className: emailGroupClass },
-
-                        React.createElement(CustomInput, { type: "email", label: emailLabel || 'Email', placeholder: emailPlaceholder, defaultLabel: "Email", handleChangeInput: this.handleChangeEmail, inputValue: email, handleFocusInput: () => this.handleFieldFocus('email'), classNameLabel: "cr-sign-in-form__label", classNameInput: "cr-sign-in-form__input", autoFocus: true }),
-
-                        emailError && React.createElement("div", { className: 'cr-sign-in-form__error' }, emailError)),
-
-                    React.createElement("div", { className: passwordGroupClass },
-
-                        React.createElement(CustomInput, { type: "password", label: passwordLabel || 'Password', placeholder: passwordPlaceholder, defaultLabel: "Password", handleChangeInput: this.handleChangePassword, inputValue: password, handleFocusInput: () => this.handleFieldFocus('password'), classNameLabel: "cr-sign-in-form__label", classNameInput: "cr-sign-in-form__input", autoFocus: false }),
-
-                        passwordError && React.createElement("div", { className: 'cr-sign-in-form__error' }, passwordError)),
-
-                    React.createElement("div", { className: "cr-sign-in-form__button-wrapper" },
-
-                        React.createElement(Button, { label: isLoading ? 'Loading...' : (labelSignIn ? labelSignIn : 'Sign in'), type: "submit", className: 'cr-sign-in-form__button', disabled: isLoading || !email.match(EMAIL_REGEX) || !password, onClick: this.handleClick })),
-
-                    React.createElement("div", { className: "cr-sign-in-form__bottom-section" },
-
-                        React.createElement("div", { className: "cr-sign-in-form__bottom-section-password", onClick: () => onForgotPassword(email) }, forgotPasswordLabel ? forgotPasswordLabel : 'Forgot your password?'))))));
-
-        
-
-    }
-
+import {
+    CustomInput,
+} from '../';
+import {
+    EMAIL_REGEX,
+} from '../../helpers';
+import {
+    Button,
+} from '../../openware';
+
+interface SignInProps {
+    labelSignIn?: string;
+    labelSignUp?: string;
+    emailLabel?: string;
+    passwordLabel?: string;
+    receiveConfirmationLabel?: string;
+    forgotPasswordLabel?: string;
+    isLoading?: boolean;
+    title?: string;
+    onForgotPassword: (email?: string) => void;
+    onConfirmationResend?: (email?: string) => void;
+    onSignUp: () => void;
+    onSignIn: () => void;
+    className?: string;
+    image?: string;
+    email: string;
+    emailError: string;
+    password: string;
+    passwordError: string;
+    emailFocused: boolean;
+    emailPlaceholder: string;
+    passwordFocused: boolean;
+    passwordPlaceholder: string;
+    isFormValid: () => void;
+    refreshError: () => void;
+    handleChangeFocusField: (value: string) => void;
+    changePassword: (value: string) => void;
+    changeEmail: (value: string) => void;
 }
 
-export { SignInComponent, };
+class SignInComponent extends React.Component<SignInProps> {
+    public render() {
+        const {
+            email,
+            emailError,
+            emailPlaceholder,
+            password,
+            passwordError,
+            passwordPlaceholder,
+            isLoading,
+            onForgotPassword,
+            onSignUp,
+            image,
+            labelSignIn,
+            labelSignUp,
+            emailLabel,
+            passwordLabel,
+            forgotPasswordLabel,
+            emailFocused,
+            passwordFocused,
+        } = this.props;
+        const emailGroupClass = cr('cr-sign-in-form__group', {
+            'cr-sign-in-form__group--focused': emailFocused,
+        });
+        const passwordGroupClass = cr('cr-sign-in-form__group', {
+            'cr-sign-in-form__group--focused': passwordFocused,
+        });
+        const logo = image ? (
+            <h1 className="cr-sign-in-form__title">
+                <img className="cr-sign-in-form__image" src={image} alt="logo" />
+            </h1>
+        ) : null;
+
+        // tslint:disable:jsx-no-lambda
+        return (
+            <form>
+                <div className="cr-sign-in-form">
+                    <div className="cr-sign-in-form__options-group">
+                        <div className="cr-sign-in-form__option">
+                            <div className="cr-sign-in-form__option-inner __selected">
+                                {labelSignIn ? labelSignIn : 'Sign In'}
+                            </div>
+                        </div>
+                        <div className="cr-sign-in-form__option">
+                            <div className="cr-sign-in-form__option-inner cr-sign-in-form__tab-signup" onClick={onSignUp}>
+                                {labelSignUp ? labelSignUp : 'Sign Up'}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cr-sign-in-form__form-content">
+                        {logo}
+                        <div className={emailGroupClass}>
+                            <CustomInput
+                                type="email"
+                                label={emailLabel || 'Email'}
+                                placeholder={emailPlaceholder}
+                                defaultLabel="Email"
+                                handleChangeInput={this.handleChangeEmail}
+                                inputValue={email}
+                                handleFocusInput={() => this.handleFieldFocus('email')}
+                                classNameLabel="cr-sign-in-form__label"
+                                classNameInput="cr-sign-in-form__input"
+                                autoFocus={true}
+                            />
+                            {emailError && <div className={'cr-sign-in-form__error'}>{emailError}</div>}
+                        </div>
+                        <div className={passwordGroupClass}>
+                            <CustomInput
+                                type="password"
+                                label={passwordLabel || 'Password'}
+                                placeholder={passwordPlaceholder}
+                                defaultLabel="Password"
+                                handleChangeInput={this.handleChangePassword}
+                                inputValue={password}
+                                handleFocusInput={() => this.handleFieldFocus('password')}
+                                classNameLabel="cr-sign-in-form__label"
+                                classNameInput="cr-sign-in-form__input"
+                                autoFocus={false}
+                            />
+                            {passwordError && <div className={'cr-sign-in-form__error'}>{passwordError}</div>}
+                        </div>
+                        <div className="cr-sign-in-form__button-wrapper">
+                            <Button
+                                label={isLoading ? 'Loading...' : (labelSignIn ? labelSignIn : 'Sign in')}
+                                type="submit"
+                                className={'cr-sign-in-form__button'}
+                                disabled={isLoading || !email.match(EMAIL_REGEX) || !password}
+                                onClick={this.handleClick}
+                            />
+                        </div>
+                        <div className="cr-sign-in-form__bottom-section">
+                            <div
+                                className="cr-sign-in-form__bottom-section-password"
+                                onClick={() => onForgotPassword(email)}
+                            >
+                                {forgotPasswordLabel ? forgotPasswordLabel : 'Forgot your password?'}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        );
+        // tslint:enable:jsx-no-lambda
+    }
+
+    private handleChangeEmail = (value: string) => {
+        this.props.changeEmail(value);
+    };
+
+    private handleChangePassword = (value: string) => {
+        this.props.changePassword(value);
+    };
+
+    private handleFieldFocus = (field: string) => {
+        this.props.handleChangeFocusField(field);
+    };
+
+    private handleSubmitForm = () => {
+        this.props.refreshError();
+        this.props.onSignIn();
+    };
+
+    private isValidForm = () => {
+        const { email, password } = this.props;
+        const isEmailValid = email.match(EMAIL_REGEX);
+
+        return email && isEmailValid && password;
+    };
+
+    private handleValidateForm = () => {
+        this.props.isFormValid();
+    };
+
+    private handleClick = (label?: string, e?: React.FormEvent<HTMLInputElement>) => {
+        if (e) {
+            e.preventDefault();
+        }
+        if (!this.isValidForm()) {
+            this.handleValidateForm();
+        } else {
+            this.handleSubmitForm();
+        }
+    };
+}
+
+export {
+    SignInComponent,
+    SignInProps,
+};
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/components/SignIn/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/components/SignIn/index.tsx
