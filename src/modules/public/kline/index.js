53e10627431885cd529fbbb0475c0d351d141bba
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:30:12 +0100

    Update index.js

--- src/modules/public/kline/index.js
+++ src/modules/public/kline/index.js
@@ -1,7 +1,14 @@
 export * from './actions';
-
 export * from './reducer';
-
 export * from './selectors';
-
+export * from './types';
 export * from './sagas';
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/kline/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/kline/index.ts
