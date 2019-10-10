5305d74d1559d2ebe5f6300c53b0db122234b100
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 22:56:20 +0100

    Update index.js

--- src/translations/index.js
+++ src/translations/index.js
@@ -1,17 +1,23 @@
 import { en } from './en';
-
+import { ja } from './ja';
+import { pt } from './pt';
 import { ru } from './ru';
 
-import { zh } from './zh';
+export type LangType = typeof en;
 
 export const languageMap = {
-
     default: en,
-
     en,
-
+    ja,
+    pt,
     ru,
-
-    zh,
-
 };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/translations/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/translations/index.ts
