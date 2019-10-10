6825a515772d7b5c8e957f596a23383da9702f9f
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 22:27:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,46 @@
-
-
+// tslint:disable:no-submodule-imports
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
-
 import { createBrowserHistory } from 'history';
-
 import * as React from 'react';
-
 import * as ReactDOM from 'react-dom';
-
 import { addLocaleData } from 'react-intl';
-
 import { Provider } from 'react-redux';
 
 import { App } from './App';
-
 import './index.css';
-
 import { rootSaga } from './modules';
-
 import { rangerSagas } from './modules/public/ranger';
-
 import { rangerMiddleware, sagaMiddleware, store } from './store';
-
-import {en} from './translations/en';
-
-import {zh} from './translations/zh';
-
-import {ru} from './translations/ru';
 
 const history = createBrowserHistory();
 
+// tslint:disable:no-submodule-imports
+import en = require('react-intl/locale-data/en');
+import ja = require('react-intl/locale-data/ja');
+import ko = require('react-intl/locale-data/ko');
+import pt = require('react-intl/locale-data/pt');
+import ru = require('react-intl/locale-data/ru');
+// tslint:enable
 
 
-
-
-addLocaleData([...en, ...ru, ...zh]);
-
+addLocaleData([...en, ...ja, ...ko, ...pt, ...ru]);
 sagaMiddleware.run(rootSaga);
-
 rangerMiddleware.run(rangerSagas);
 
-const render = () => ReactDOM.render(React.createElement(Provider, { store: store },
-
-    React.createElement(App, { history: history })), document.getElementById('root'));
+const render = () => ReactDOM.render(
+    <Provider store={store}>
+        <App history={history} />
+    </Provider>,
+    document.getElementById('root') as HTMLElement,
+);
 
 render();
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/index.tsx
