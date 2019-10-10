b5f4b1df5413400515bee4d332b8e007a3717c6c
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:12:13 +0100

    Update actions.js

--- src/modules/user/history/actions.js
+++ src/modules/user/history/actions.js
@@ -1,47 +1,99 @@
-import { HISTORY_DATA, HISTORY_ERROR, HISTORY_FETCH, HISTORY_PUSH_EMIT, HISTORY_PUSH_FINISH, HISTORY_RESET, } from './constants';
+import {
+    HISTORY_DATA,
+    HISTORY_ERROR,
+    HISTORY_FETCH,
+    HISTORY_PUSH_EMIT,
+    HISTORY_PUSH_FINISH,
+    HISTORY_RESET,
+} from './constants';
+import { PrivateTradeEvent, WalletHistoryList } from './types';
 
-export const fetchHistory = (payload) => ({
+interface HistoryFetchPayload {
+    currency?: string;
+    page: number;
+    type: string;
+    limit: number;
+    market?: string;
+}
 
+interface HistorySuccessPayload {
+    list: WalletHistoryList;
+    page: number;
+    fullHistory: number;
+}
+
+export interface HistoryFetch {
+    type: typeof HISTORY_FETCH;
+    payload: HistoryFetchPayload;
+}
+
+export interface HistoryData {
+    type: typeof HISTORY_DATA;
+    payload: HistorySuccessPayload;
+}
+
+export interface HistoryError {
+    type: typeof HISTORY_ERROR;
+    payload: WalletHistoryList;
+}
+
+export interface HistoryPushFinish {
+    type: typeof HISTORY_PUSH_FINISH;
+    payload: WalletHistoryList;
+}
+
+export interface HistoryReset {
+    type: typeof HISTORY_RESET;
+}
+
+export interface HistoryPush {
+    type: typeof HISTORY_PUSH_EMIT;
+    payload: PrivateTradeEvent;
+}
+
+export type HistoryActions =
+    HistoryFetch
+    | HistoryData
+    | HistoryError
+    | HistoryReset
+    | HistoryPush
+    | HistoryPushFinish;
+
+
+export const fetchHistory = (payload: HistoryFetchPayload): HistoryFetch => ({
     type: HISTORY_FETCH,
-
     payload,
-
 });
 
-export const successHistory = (payload) => ({
-
+export const successHistory = (payload: HistorySuccessPayload): HistoryData => ({
     type: HISTORY_DATA,
-
     payload,
-
 });
 
-export const failHistory = (payload) => ({
-
+export const failHistory = (payload: WalletHistoryList): HistoryError => ({
     type: HISTORY_ERROR,
-
     payload,
-
 });
 
-export const resetHistory = () => ({
-
+export const resetHistory = (): HistoryReset => ({
     type: HISTORY_RESET,
-
 });
 
-export const pushHistoryEmit = (payload) => ({
-
+export const pushHistoryEmit = (payload: PrivateTradeEvent): HistoryPush => ({
     type: HISTORY_PUSH_EMIT,
-
     payload,
-
 });
 
-export const pushHistoryFinish = (payload) => ({
-
+export const pushHistoryFinish = (payload: WalletHistoryList): HistoryPushFinish => ({
     type: HISTORY_PUSH_FINISH,
-
     payload,
-
 });
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/history/actions.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/history/actions.ts
