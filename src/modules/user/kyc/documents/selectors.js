436991f040ccbfaa673f1fd6ef2b6f1e324faf6e
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:18:56 +0100

    Update selectors.js

--- src/modules/user/kyc/documents/selectors.js
+++ src/modules/user/kyc/documents/selectors.js
@@ -1,3 +1,17 @@
-export const selectSendDocumentsSuccess = (state) => state.user.documents.success;
+import { RootState } from '../../../index';
+import { DocumentsState } from './reducer';
 
-export const selectSendDocumentsLoading = (state) => state.user.documents.loading;
+export const selectSendDocumentsSuccess = (state: RootState): DocumentsState['success'] =>
+    state.user.documents.success;
+
+export const selectSendDocumentsLoading = (state: RootState): DocumentsState['loading'] =>
+    state.user.documents.loading;
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/kyc/documents/selectors.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/kyc/documents/selectors.ts
