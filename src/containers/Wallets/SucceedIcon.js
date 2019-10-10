ed65e7b2c319bea47bef68858bf6f4d2507a48b6
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:57:09 +0100

    Update SucceedIcon.js

--- src/containers/Wallets/SucceedIcon.js
+++ src/containers/Wallets/SucceedIcon.js
@@ -1,9 +1,25 @@
 import * as React from 'react';
 
+
 export const SucceedIcon = () => {
-
-    return (React.createElement("svg", { width: "10", height: "8", viewBox: "0 0 10 8", fill: "none", xmlns: "http://www.w3.org/2000/svg" },
-
-        React.createElement("path", { d: "M9 1L3.68517 7L1 4.47371", stroke: "#737F92", strokeWidth: "2", strokeLinecap: "round", strokeLinejoin: "round" })));
-
+    return (
+        <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M9 1L3.68517 7L1 4.47371"
+                stroke="#737F92"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    );
 };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/containers/Wallets/SucceedIcon.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/containers/Wallets/SucceedIcon.tsx
