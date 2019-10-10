751e0cf881a5a0dd0eb3fdb9f8372fb586559e33
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:18:18 +0100

    Update reducer.js

--- src/modules/user/kyc/documents/reducer.js
+++ src/modules/user/kyc/documents/reducer.js
@@ -1,55 +1,51 @@
-import { SEND_DOCUMENTS_DATA, SEND_DOCUMENTS_ERROR, SEND_DOCUMENTS_FETCH, } from './constants';
+import { CommonError } from '../../../types';
+import { DocumentsAction } from './actions';
+import {
+    SEND_DOCUMENTS_DATA,
+    SEND_DOCUMENTS_ERROR,
+    SEND_DOCUMENTS_FETCH,
+} from './constants';
 
-export const initialDocumentsState = { loading: false };
+export interface DocumentsState {
+    success?: string;
+    error?: CommonError;
+    loading: boolean;
+}
 
-export const documentsReducer = (state = initialDocumentsState, action) => {
+export const initialDocumentsState: DocumentsState = { loading: false };
 
+export const documentsReducer = (state = initialDocumentsState, action: DocumentsAction) => {
     switch (action.type) {
-
         case SEND_DOCUMENTS_FETCH:
-
             return {
-
                 ...state,
-
                 success: undefined,
-
                 error: undefined,
-
                 loading: true,
-
             };
-
         case SEND_DOCUMENTS_DATA:
-
             return {
-
                 ...state,
-
                 success: action.payload.message,
-
                 error: undefined,
-
                 loading: false,
-
             };
-
         case SEND_DOCUMENTS_ERROR:
-
             return {
-
                 success: undefined,
-
                 error: action.payload,
-
                 loading: false,
-
             };
-
         default:
-
             return state;
-
     }
-
 };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/kyc/documents/reducer.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/kyc/documents/reducer.ts
