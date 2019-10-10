d1c8f36148a742b73c1808df31e47a894616c388
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:55:45 +0100

    Update index.js

--- src/containers/Wallets/EstimatedValue/index.js
+++ src/containers/Wallets/EstimatedValue/index.js
@@ -1,109 +1,110 @@
-import { Decimal } from '@openware/components';
-
 import * as React from 'react';
-
-import { injectIntl } from 'react-intl';
-
-import { connect } from 'react-redux';
-
+import { InjectedIntlProps, injectIntl } from 'react-intl';
+import { connect, MapDispatchToPropsFunction } from 'react-redux';
+import { WalletItemProps } from '../../../components/WalletItem';
 import { estimateValue, estimateWithMarket, findPrecision } from '../../../helpers/estimateValue';
-
-import { marketsFetch, selectMarkets, selectMarketTickers, selectUserLoggedIn, } from '../../../modules';
-
-import { rangerConnectFetch } from '../../../modules/public/ranger';
-
+import {
+    marketsFetch,
+    RootState,
+    selectMarkets,
+    selectMarketTickers, selectUserLoggedIn,
+} from '../../../modules';
+import { Market, Ticker } from '../../../modules/public/markets';
+import { rangerConnectFetch, RangerConnectFetch } from '../../../modules/public/ranger';
+import { RangerState } from '../../../modules/public/ranger/reducer';
 import { selectRanger } from '../../../modules/public/ranger/selectors';
+import { Decimal } from '../../../openware';
 
-class EstimatedValueContainer extends React.Component {
+interface EstimatedValueProps {
+    wallets: WalletItemProps[];
+    hello: string;
+}
 
-    constructor() {
+interface ReduxProps {
+    markets: Market[];
+    marketTickers: {
+        [key: string]: Ticker,
+    };
+    rangerState: RangerState;
+    userLoggedIn: boolean;
+}
 
-        super(...arguments);
+interface DispatchProps {
+    marketsFetch: typeof marketsFetch;
+    rangerConnect: typeof rangerConnectFetch;
+}
 
-        this.t = (key) => {
+type Props = DispatchProps & ReduxProps & EstimatedValueProps & InjectedIntlProps;
 
-            return this.props.intl.formatMessage({ id: key });
-
-        };
-
-    }
-
-    componentDidMount() {
-
-        const { markets, userLoggedIn, rangerState: { connected } } = this.props;
+class EstimatedValueContainer extends React.Component<Props> {
+    public componentDidMount(): void {
+        const {markets, userLoggedIn, rangerState: {connected}} = this.props;
 
         if (markets.length < 1) {
-
             this.props.marketsFetch();
-
         }
 
         if (!connected) {
-
-            this.props.rangerConnect({ withAuth: userLoggedIn });
-
+            this.props.rangerConnect({withAuth: userLoggedIn});
         }
-
     }
 
-    render() {
-
-        const { wallets, markets, marketTickers } = this.props;
-
+    public render(): React.ReactNode {
+        const {wallets, markets, marketTickers} = this.props;
         const primaryCurrency = 'usd';
-
         const primaryCurrencyPrecision = findPrecision(primaryCurrency, markets);
-
         const estimatedValue = estimateValue(primaryCurrency, wallets, markets, marketTickers);
-
         const secondaryCurrency = 'eth';
-
         const secondaryCurrencyPrecision = findPrecision(secondaryCurrency, markets);
-
         const estimatedValueSecondary = estimateWithMarket(secondaryCurrency, primaryCurrency, estimatedValue, markets, marketTickers);
 
-        return (React.createElement("div", { className: "pg-estimated-value" },
+        return (
+            <div className="pg-estimated-value">
+                <div className="pg-estimated-value__container">
+                    {this.t('page.body.wallets.estimated_value')}
+                    <span className="value-container">
+                        <span className="value">
+                            {Decimal.format(estimatedValue, primaryCurrencyPrecision)}
+                        </span>
+                        <span className="value-sign">{primaryCurrency.toUpperCase()}</span>
+                    </span>
 
-            React.createElement("div", { className: "pg-estimated-value__container" },
-
-                this.t('page.body.wallets.estimated_value'),
-
-                React.createElement("span", { className: "value-container" },
-
-                    React.createElement("span", { className: "value" }, Decimal.format(estimatedValue, primaryCurrencyPrecision)),
-
-                    React.createElement("span", { className: "value-sign" }, primaryCurrency.toUpperCase())),
-
-                React.createElement("span", { className: "value-container" },
-
-                    React.createElement("span", { className: "value" }, Decimal.format(estimatedValueSecondary, secondaryCurrencyPrecision)),
-
-                    React.createElement("span", { className: "value-sign" }, secondaryCurrency.toUpperCase())))));
-
+                    <span className="value-container">
+                        <span className="value">
+                            {Decimal.format(estimatedValueSecondary, secondaryCurrencyPrecision)}
+                        </span>
+                        <span className="value-sign">{secondaryCurrency.toUpperCase()}</span>
+                    </span>
+                </div>
+            </div>
+        );
     }
 
+    public t = (key: string) => {
+        return this.props.intl.formatMessage({id: key});
+    };
 }
 
-const mapStateToProps = (state) => ({
-
+const mapStateToProps = (state: RootState): ReduxProps => ({
     markets: selectMarkets(state),
-
     marketTickers: selectMarketTickers(state),
-
     rangerState: selectRanger(state),
-
     userLoggedIn: selectUserLoggedIn(state),
-
 });
 
-const mapDispatchToProps = dispatch => ({
-
+const mapDispatchToProps: MapDispatchToPropsFunction<DispatchProps, {}> = dispatch => ({
     marketsFetch: () => dispatch(marketsFetch()),
-
-    rangerConnect: (payload) => dispatch(rangerConnectFetch(payload)),
-
+    rangerConnect: (payload: RangerConnectFetch['payload']) => dispatch(rangerConnectFetch(payload)),
 });
 
+// tslint:disable-next-line:no-any
+export const EstimatedValue = injectIntl(connect(mapStateToProps, mapDispatchToProps)(EstimatedValueContainer)) as any;
 
 
-export const EstimatedValue = injectIntl(connect(mapStateToProps, mapDispatchToProps)(EstimatedValueContainer));
+// WEBPACK FOOTER //
+// src/drone/src/src/containers/Wallets/EstimatedValue/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/containers/Wallets/EstimatedValue/index.tsx
