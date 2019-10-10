f722d9255a4e815d4437fe678ae6a3da7dcf1908
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:53:54 +0100

    Update selectors.js

--- src/modules/user/apiKeys/selectors.js
+++ src/modules/user/apiKeys/selectors.js
@@ -1,5 +1,16 @@
-export const selectApiKeys = (state) => state.user.apiKeys.apiKeys;
+import { ApiKeys2FAModal, ApiKeysState, RootState } from '../../index';
 
-export const selectApiKeysDataLoaded = (state) => state.user.apiKeys.dataLoaded;
+export const selectApiKeys = (state: RootState): ApiKeysState['apiKeys'] => state.user.apiKeys.apiKeys;
 
-export const selectApiKeysModal = (state) => state.user.apiKeys.modal;
+export const selectApiKeysDataLoaded = (state: RootState): ApiKeysState['dataLoaded'] => state.user.apiKeys.dataLoaded;
+
+export const selectApiKeysModal = (state: RootState): ApiKeys2FAModal['payload'] => state.user.apiKeys.modal;
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/apiKeys/selectors.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/apiKeys/selectors.ts
