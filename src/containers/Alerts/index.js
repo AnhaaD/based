43e2ad5fe6ad8ef92fba77bcc1985be3975ea8c3
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:27:17 +0100

    Update index.js

--- src/containers/Alerts/index.js
+++ src/containers/Alerts/index.js
@@ -1,61 +1,66 @@
-import { Alert } from '@openware/components';
-
 import * as React from 'react';
-
 import FadeIn from 'react-fade-in';
+import {
+    InjectedIntlProps,
+    injectIntl,
+} from 'react-intl';
+import { connect, MapDispatchToPropsFunction } from 'react-redux';
+import {
+    alertDelete,
+    alertDeleteByIndex,
+    AlertState,
+    RootState,
+    selectAlertState,
+} from '../../modules';
+import { Alert } from '../../openware';
 
-import { injectIntl, } from 'react-intl';
-
-import { connect } from 'react-redux';
-
-import { alertDelete, alertDeleteByIndex, selectAlertState, } from '../../modules';
-
-class AlertComponent extends React.Component {
-
-    constructor() {
-
-        super(...arguments);
-
-        this.deleteAlertByIndex = (key) => {
-
-            this.props.alertDeleteByIndex(key);
-
-        };
-
-        this.translate = (id) => {
-
-            return id ? this.props.intl.formatMessage({ id }) : '';
-
-        };
-
-    }
-
-    
-
-    render() {
-
-        return (React.createElement("div", { className: "pg-alerts" }, this.props.alerts.alerts.map(w => w.message.map((msg, index) => React.createElement(FadeIn, { key: index },
-
-            React.createElement("div", { onClick: () => this.deleteAlertByIndex(index) },
-
-                React.createElement(Alert, { description: w.code && w.code.toString(10), title: this.translate(msg), type: w.type })))))));
-
-    }
-
+interface ReduxProps {
+    alerts: AlertState;
 }
 
-const mapStateToProps = (state) => ({
+interface DispatchProps {
+    alertDelete: typeof alertDelete;
+    alertDeleteByIndex: typeof alertDeleteByIndex;
+}
 
+type Props = ReduxProps & DispatchProps & InjectedIntlProps;
+
+class AlertComponent extends React.Component<Props> {
+    public deleteAlertByIndex = (key: number) => {
+        this.props.alertDeleteByIndex(key);
+    };
+
+    public translate = (id: string) => {
+        return id ? this.props.intl.formatMessage({ id }) : '';
+    };
+
+    // tslint:disable jsx-no-lambda
+    public render() {
+        return (
+            <div className="pg-alerts">
+                {this.props.alerts.alerts.map(w => w.message.map((msg, index) => <FadeIn key={index}><div onClick={() => this.deleteAlertByIndex(index)}><Alert description={w.code && w.code.toString(10)} title={this.translate(msg)} type={w.type} /></div></FadeIn>))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state: RootState): ReduxProps => ({
     alerts: selectAlertState(state),
-
 });
 
-const mapDispatchToProps = dispatch => ({
-
-    alertDelete: () => dispatch(alertDelete()),
-
-    alertDeleteByIndex: payload => dispatch(alertDeleteByIndex(payload)),
-
-});
+const mapDispatchToProps: MapDispatchToPropsFunction<DispatchProps, {}> =
+    dispatch => ({
+        alertDelete: () => dispatch(alertDelete()),
+        alertDeleteByIndex: payload => dispatch(alertDeleteByIndex(payload)),
+    });
 
 export const Alerts = injectIntl(connect(mapStateToProps, mapDispatchToProps)(AlertComponent));
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/containers/Alerts/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/containers/Alerts/index.tsx
