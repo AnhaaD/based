e81baf7ec3518f05e2a331560896967ebdc64e51
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:45:32 +0100

    Update constants.js

--- src/modules/public/recentTrades/constants.js
+++ src/modules/public/recentTrades/constants.js
@@ -1,7 +1,13 @@
 export const RECENT_TRADES_FETCH = 'recentTrades/FETCH';
-
 export const RECENT_TRADES_DATA = 'recentTrades/DATA';
-
 export const RECENT_TRADES_ERROR = 'recentTrades/ERROR';
-
 export const RECENT_TRADES_PUSH = 'recentTrades/PUSH';
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/recentTrades/constants.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/recentTrades/constants.ts
