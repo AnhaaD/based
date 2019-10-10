6b6bae3b0ecbf448d9e9974662ed8dd68fc4c1d0
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:04:38 +0100

    Update index.js

--- src/screens/OrdersTabScreen/index.js
+++ src/screens/OrdersTabScreen/index.js
@@ -1,125 +1,120 @@
 import * as React from 'react';
-
-import { FormattedMessage, injectIntl, } from 'react-intl';
-
-import { connect } from 'react-redux';
-
+import {
+    FormattedMessage,
+    InjectedIntlProps,
+    injectIntl,
+} from 'react-intl';
+import { connect, MapDispatchToPropsFunction } from 'react-redux';
 import { TabPanel } from '../../components';
-
 import { OrdersElement } from '../../containers/OrdersElement';
-
 import { setDocumentTitle } from '../../helpers';
+import {
+    marketsFetch,
+    ordersCancelAllFetch,
+    RootState,
+    selectOrdersHistory,
+} from '../../modules';
+import { OrderCommon } from '../../modules/types';
 
-import { marketsFetch, ordersCancelAllFetch, resetOrdersHistory, selectOrdersHistory, } from '../../modules';
-
-class Orders extends React.PureComponent {
-
-    constructor() {
-
-        super(...arguments);
-
-        this.state = { tab: 'open', currentTabIndex: 0 };
-
-        this.tabMapping = ['open', 'all'];
-
-        this.onCurrentTabChange = index => this.setState({ currentTabIndex: index });
-
-        this.handleMakeRequest = (index) => {
-
-            this.renderTabs();
-
-            if (this.state.tab === this.tabMapping[index]) {
-
-                return;
-
-            }
-
-            this.props.resetOrdersHistory();
-
-            this.setState({ tab: this.tabMapping[index] });
-
-        };
-
-        this.renderTabs = () => {
-
-            const { tab } = this.state;
-
-            return [
-
-                {
-
-                    content: tab === 'open' ? React.createElement(OrdersElement, { type: "open" }) : null,
-
-                    label: this.props.intl.formatMessage({ id: 'page.body.openOrders.tab.open' }),
-
-                },
-
-                {
-
-                    content: tab === 'all' ? React.createElement(OrdersElement, { type: "all" }) : null,
-
-                    label: this.props.intl.formatMessage({ id: 'page.body.openOrders.tab.all' }),
-
-                },
-
-            ];
-
-        };
-
-        this.handleCancelAll = () => this.props.ordersCancelAll(this.state);
-
-    }
-
-    componentDidMount() {
-
-        setDocumentTitle('Orders');
-
-        this.props.marketsFetch();
-
-    }
-
-    componentWillUnmount() {
-
-        this.props.resetOrdersHistory();
-
-    }
-
-    render() {
-
-        const cancelAll = this.props.list.length ? (React.createElement(React.Fragment, null,
-
-            React.createElement("span", { onClick: this.handleCancelAll },
-
-                React.createElement(FormattedMessage, { id: "page.body.openOrders.header.button.cancelAll" }),
-
-                React.createElement("span", { className: "pg-orders-tab__close" })))) : null;
-
-        return (React.createElement("div", { className: "pg-orders-tab pg-container" },
-
-            React.createElement("div", { className: "pg-orders-tab__tabs-content" },
-
-                React.createElement(TabPanel, { panels: this.renderTabs(), onTabChange: this.handleMakeRequest, optionalHead: cancelAll, currentTabIndex: this.state.currentTabIndex, onCurrentTabChange: this.onCurrentTabChange }))));
-
-    }
-
+interface ReduxProps {
+    list: OrderCommon[];
 }
 
-const mapStateToProps = (state) => ({
+interface DispatchProps {
+    marketsFetch: typeof marketsFetch;
+    ordersCancelAll: typeof ordersCancelAllFetch;
+}
 
+type Props = ReduxProps & DispatchProps & InjectedIntlProps;
+
+interface State {
+    tab: string;
+    currentTabIndex: number;
+}
+
+class Orders extends React.PureComponent<Props, State> {
+    public state = { tab: 'open', currentTabIndex: 0};
+
+    public tabMapping = ['open', 'all'];
+
+    public componentDidMount() {
+        setDocumentTitle('Orders');
+        this.props.marketsFetch();
+    }
+
+    public render() {
+        const cancelAll = this.props.list.length ? (
+            <React.Fragment>
+                <span onClick={this.handleCancelAll}>
+                    <FormattedMessage id="page.body.openOrders.header.button.cancelAll" />
+                    <span className="pg-orders-tab__close" />
+                </span>
+            </React.Fragment>
+        ) : null;
+
+        return (
+            <div className="pg-orders-tab pg-container">
+                <div className="pg-orders-tab__tabs-content">
+                    <TabPanel
+                        panels={this.renderTabs()}
+                        onTabChange={this.handleMakeRequest}
+                        optionalHead={cancelAll}
+                        currentTabIndex={this.state.currentTabIndex}
+                        onCurrentTabChange={this.onCurrentTabChange}
+                    />
+                </div>
+            </div>
+        );
+    }
+
+    private onCurrentTabChange = index => this.setState({ currentTabIndex: index });
+
+    private handleMakeRequest = (index: number) => {
+        this.renderTabs();
+        if (this.state.tab === this.tabMapping[index]) {
+            return;
+        }
+        this.setState({ tab: this.tabMapping[index] });
+    };
+
+    private renderTabs = () => {
+        const { tab } = this.state;
+        return [
+            {
+                content: tab === 'open' ? <OrdersElement type="open"/> : null,
+                label: this.props.intl.formatMessage({ id: 'page.body.openOrders.tab.open'}),
+            },
+            {
+                content: tab === 'all' ? <OrdersElement type="all" /> : null,
+                label: this.props.intl.formatMessage({ id: 'page.body.openOrders.tab.all'}),
+            },
+        ];
+    };
+
+    private handleCancelAll = () => this.props.ordersCancelAll(this.state);
+}
+
+const mapStateToProps = (state: RootState): ReduxProps => ({
     list: selectOrdersHistory(state),
-
 });
 
-const mapDispatchToProps = dispatch => ({
-
-    marketsFetch: () => dispatch(marketsFetch()),
-
-    ordersCancelAll: payload => dispatch(ordersCancelAllFetch(payload)),
-
-    resetOrdersHistory: () => dispatch(resetOrdersHistory()),
-
-});
+const mapDispatchToProps: MapDispatchToPropsFunction<DispatchProps, {}> =
+    dispatch => ({
+        marketsFetch: () => dispatch(marketsFetch()),
+        ordersCancelAll: payload => dispatch(ordersCancelAllFetch(payload)),
+    });
 
 const OrdersTabScreen = injectIntl(connect(mapStateToProps, mapDispatchToProps)(Orders));
 
-export { OrdersTabScreen, };
+export {
+    OrdersTabScreen,
+};
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/screens/OrdersTabScreen/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/screens/OrdersTabScreen/index.tsx
