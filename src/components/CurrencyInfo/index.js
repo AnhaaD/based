ee3e138cec05dd0e393b9b74c1ef67756709d2ef
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:10:31 +0100

    Update index.js

--- src/components/CurrencyInfo/index.js
+++ src/components/CurrencyInfo/index.js
@@ -1,65 +1,70 @@
-import { Decimal } from '@openware/components';
-
 import * as React from 'react';
-
 import { FormattedMessage } from 'react-intl';
+import { Decimal } from '../../openware';
+import { WalletItemProps } from '../WalletItem';
 
-const CurrencyIcon = (props) => {
 
+interface CurrencyInfoProps {
+    wallet: WalletItemProps;
+}
+
+interface CurrencyIconProps {
+    icon?: string | null;
+    currency: string;
+}
+
+const CurrencyIcon: React.FunctionComponent<CurrencyIconProps> = (props: CurrencyIconProps) => {
     return props.icon ?
-
-        React.createElement("img", { className: "cr-wallet-item__single__image-icon", src: props.icon }) :
-
-        React.createElement("span", { className: `cr-wallet-item__icon-code cr-crypto-font-${props.currency}` });
-
+        <img className="cr-wallet-item__single__image-icon" src={props.icon} /> :
+        <span className={`cr-wallet-item__icon-code cr-crypto-font-${props.currency}`} />;
 };
 
-const CurrencyInfo = (props) => {
-
+const CurrencyInfo: React.FunctionComponent<CurrencyInfoProps> = (props: CurrencyInfoProps) => {
     const balance = (props.wallet || { balance: 0 }).balance.toString();
-
     const lockedAmount = (props.wallet || { locked: 0 }).locked;
-
     const currency = (props.wallet || { currency: '' }).currency.toUpperCase();
-
     const selectedFixed = (props.wallet || { fixed: 0 }).fixed;
 
     const stringLocked = lockedAmount ? lockedAmount.toString() : undefined;
-
     const iconUrl = props.wallet ? props.wallet.iconUrl : null;
 
-    return (React.createElement("div", { className: "cr-wallet-item__single" },
 
-        React.createElement(CurrencyIcon, { icon: iconUrl, currency: currency }),
-
-        React.createElement("div", { className: "cr-wallet-item__single-balance" },
-
-            React.createElement("div", null,
-
-                React.createElement("div", { className: "cr-wallet-item__amount-locked" },
-
-                    React.createElement(FormattedMessage, { id: "page.body.wallets.locked" })),
-
-                React.createElement("span", { className: "cr-wallet-item__balance-locked" },
-
-                    React.createElement(Decimal, { fixed: selectedFixed }, stringLocked))),
-
-            React.createElement("div", null,
-
-                React.createElement("span", { className: "cr-wallet-item__balance" },
-
-                    currency,
-
-                    "\u00A0",
-
-                    React.createElement(FormattedMessage, { id: "page.body.wallets.balance" })),
-
-                "\u00A0",
-
-                React.createElement("span", { className: "cr-wallet-item__balance-amount" },
-
-                    React.createElement(Decimal, { fixed: selectedFixed }, balance))))));
-
+    return (
+        <div className="cr-wallet-item__single">
+            <CurrencyIcon icon={iconUrl} currency={currency}/>
+            <div className="cr-wallet-item__single-balance">
+                <div>
+                    <div className="cr-wallet-item__amount-locked">
+                        <FormattedMessage id="page.body.wallets.locked" />
+                    </div>
+                    <span className="cr-wallet-item__balance-locked">
+                    <Decimal fixed={selectedFixed}>{stringLocked}</Decimal>
+                </span>
+                </div>
+                <div>
+                <span className="cr-wallet-item__balance">
+                    {currency}&nbsp;<FormattedMessage id="page.body.wallets.balance"/>
+                </span>
+                &nbsp;
+                <span className="cr-wallet-item__balance-amount">
+                    <Decimal fixed={selectedFixed}>{balance}</Decimal>
+                </span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
-export { CurrencyInfo, };
+export {
+    CurrencyInfo,
+    CurrencyInfoProps,
+};
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/components/CurrencyInfo/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/components/CurrencyInfo/index.tsx
