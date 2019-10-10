561910c6b0c9267a46928c0a2cf360b6ec0d78e6
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:16:28 +0100

    Update index.js

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -1,57 +1,61 @@
-import { Table } from '@openware/components';
-
 import classnames from 'classnames';
-
 import * as React from 'react';
+import { CellData, Table } from '../../openware';
 
-class History extends React.PureComponent {
-
-    constructor() {
-
-        super(...arguments);
-
-        this.defaultHeaders = ['Time', 'Action', 'Price', 'Amount', 'Total'];
-
-        this.title = 'Trades History';
-
-        this.mapRows = (cell, index) => {
-
-            const { headers = this.defaultHeaders } = this.props;
-
-            const actionIndex = headers.findIndex(header => header === 'Action');
-
-            return index === actionIndex ? this.renderAction(cell) : cell;
-
-        };
-
-    }
-
-    render() {
-
-        const { headers = this.defaultHeaders } = this.props;
-
-        const tableData = this.props.data.map(row => row.map(this.mapRows));
-
-        return (React.createElement(Table, { data: tableData, header: headers, titleComponent: this.title }));
-
-    }
-
-    renderAction(actionType) {
-
-        const action = actionType ? actionType.toLowerCase() : actionType;
-
-        const className = classnames('cr-history-action', {
-
-            'cr-history-action--buy': action === 'bid',
-
-            'cr-history-action--sell': action === 'ask',
-
-        });
-
-        return React.createElement("span", { className: className }, action);
-
-    }
-
+interface HistoryProps {
+    /**
+     * List of history data
+     */
+    data: CellData[][];
+    /**
+     * List of headers for history table
+     */
+    headers?: string[];
 }
 
-export { History, };
+class History extends React.PureComponent<HistoryProps> {
+    private defaultHeaders = ['Time', 'Action', 'Price', 'Amount', 'Total'];
+    private title = 'Trades History';
+
+    public render() {
+        const { headers = this.defaultHeaders } = this.props;
+        const tableData = this.props.data.map(row => row.map(this.mapRows));
+        return (
+            <Table
+                data={tableData}
+                header={headers}
+                titleComponent={this.title}
+            />
+        );
+    }
+
+    public renderAction(actionType: string) {
+        const action = actionType ? actionType.toLowerCase() : actionType;
+        const className = classnames('cr-history-action', {
+            'cr-history-action--buy': action === 'bid',
+            'cr-history-action--sell': action === 'ask',
+        });
+
+        return <span className={className}>{action}</span>;
+    }
+
+    private mapRows = (cell: CellData, index: number) => {
+        const { headers = this.defaultHeaders } = this.props;
+        const actionIndex = headers.findIndex(header => header === 'Action');
+        return index === actionIndex ? this.renderAction(cell as string) : cell;
+    }
+}
+
+export {
+    History,
+    HistoryProps,
+};
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/components/History/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/components/History/index.tsx
