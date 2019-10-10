df80bd8e97992594ce8bf9a1ba0c1f6d40b2787a
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 23:27:44 +0100

    Update selectors.js

--- src/modules/public/i18n/selectors.js
+++ src/modules/public/i18n/selectors.js
@@ -1,1 +1,14 @@
-export const selectCurrentLanguage = (state) => state.public.i18n.lang;
+import { RootState } from '../../index';
+import { LanguageState } from './reducer';
+
+export const selectCurrentLanguage = (state: RootState): LanguageState['lang'] =>
+    state.public.i18n.lang;
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/public/i18n/selectors.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/public/i18n/selectors.ts
