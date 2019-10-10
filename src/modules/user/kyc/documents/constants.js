ea60633fbd98d290d855db7d250cc759e8f3b341
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:17:03 +0100

    Update constants.js

--- src/modules/user/kyc/documents/constants.js
+++ src/modules/user/kyc/documents/constants.js
@@ -1,5 +1,12 @@
 export const SEND_DOCUMENTS_ERROR = 'documents/SEND_DOCUMENTS_ERROR';
-
 export const SEND_DOCUMENTS_FETCH = 'documents/SEND_DOCUMENTS_FETCH';
-
 export const SEND_DOCUMENTS_DATA = 'documents/SEND_DOCUMENTS_DATA';
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/kyc/documents/constants.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/kyc/documents/constants.ts
