8218bc860f069a8e20f1bc156c099d71fb3136fc
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-11 00:11:40 +0100

    Update index.js

--- src/modules/user/history/sagas/index.js
+++ src/modules/user/history/sagas/index.js
@@ -1,15 +1,19 @@
+// tslint:disable-next-line
 import { takeLatest } from 'redux-saga/effects';
-
 import { HISTORY_FETCH, HISTORY_PUSH_EMIT } from '../constants';
-
 import { historyPushSaga } from './historyPushSaga';
-
 import { historySaga } from './historySaga';
 
 export function* rootHistorySaga() {
-
     yield takeLatest(HISTORY_PUSH_EMIT, historyPushSaga);
-
     yield takeLatest(HISTORY_FETCH, historySaga);
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/history/sagas/index.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/history/sagas/index.ts
