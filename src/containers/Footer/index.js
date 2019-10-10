3e346be143cdd5ba4208f0e40e99519ea77dcfe2
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:31:09 +0100

    Update index.js

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -1,35 +1,61 @@
-import { Button } from '@openware/components';
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Link, withRouter } from 'react-router-dom';
+import { Button } from '../../openware';
 import { LegalDocuments } from '../LegalDocuments';
-class FooterComponent extends React.Component {
-    constructor() {
-        super(...arguments);
-        this.state = {
-            showModal: false,
-        };
-        this.toggleModal = () => {
-            this.setState({
-                showModal: !this.state.showModal,
-            });
-        };
-    }
-    render() {
-        const { showModal } = this.state;
-        // eslint-disable-next-line no-restricted-globals
-        if (location.pathname.startsWith('/confirm')) {
-            return React.createElement(React.Fragment, null);
-        }
-        return (React.createElement(React.Fragment, null,
-            React.createElement("footer", { className: "pg-footer" },
-                React.createElement("span", { className: "pg-footer__link", onClick: this.toggleModal },
-                    React.createElement(FormattedMessage, { id: "page.footer.legalDocuments" })),
-                React.createElement(LegalDocuments, { isOpen: showModal, footer: React.createElement(Button, { label: 'Ok', onClick: this.toggleModal }) }),
-                React.createElement(Link, { className: "pg-footer__link", to: "/help" },
-                    React.createElement(FormattedMessage, { id: "page.footer.faq" })))));
-    }
+
+export interface FooterState {
+    showModal: boolean;
 }
 
-const Footer = withRouter(FooterComponent);
-export { Footer, };
+class FooterComponent extends React.Component<{}, FooterState> {
+    public state = {
+        showModal: false,
+    };
+
+    public render() {
+        const { showModal } = this.state;
+        if (location.pathname.startsWith('/confirm')) {
+            return <React.Fragment />;
+        }
+
+        return (
+            <React.Fragment>
+                <footer className="pg-footer">
+                    <span className="pg-footer__link" onClick={this.toggleModal}>
+                        <FormattedMessage id="page.footer.legalDocuments" />
+                    </span>
+                    <LegalDocuments
+                        isOpen={showModal}
+                        footer={<Button label={'Ok'} onClick={this.toggleModal} />}
+                    />
+                    <Link className="pg-footer__link" to="/help">
+                        <FormattedMessage id="page.footer.faq" />
+                    </Link>
+                </footer>
+            </React.Fragment>
+        );
+    }
+
+    private toggleModal = () => {
+        this.setState({
+            showModal: !this.state.showModal,
+        });
+    };
+}
+
+// tslint:disable-next-line:no-any
+const Footer = withRouter(FooterComponent as any);
+
+export {
+    Footer,
+};
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/containers/Footer/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/containers/Footer/index.tsx
