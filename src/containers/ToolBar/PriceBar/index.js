118e846868ba10c4b68a255862042aaebdd7ea90
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-10 21:51:37 +0100

    Update index.js

--- src/containers/ToolBar/PriceBar/index.js
+++ src/containers/ToolBar/PriceBar/index.js
@@ -1,43 +1,41 @@
 import * as React from 'react';
 
-export class PriceBar extends React.Component {
-
-    render() {
-
-        const { percentage, lastPrice, bidUnit } = this.props;
-
-        const gap = 18;
-
-        const position = percentage > 50 ? 'right' : 'left';
-
-        let positionValue;
-
-        positionValue = position === 'left' ? percentage < gap ? '0' : `${percentage - gap}%` : percentage + gap > 100 ? '0' : `${100 - percentage - gap}%`;
-
-        const style = {};
-
-        style[position] = positionValue;
-
-        return (React.createElement("div", { className: "pg-trading-header-price-bar" },
-
-            React.createElement("div", { className: "pg-trading-header-price-bar-filler" },
-
-                React.createElement("div", { className: "pg-trading-header-price-bar-filler-left", style: { width: `${percentage}%` } }),
-
-                React.createElement("div", { className: "pg-trading-header-price-bar-filler-cursor" }),
-
-                React.createElement("div", { className: "pg-trading-header-price-bar-text", style: style },
-
-                    "Last Price: ",
-
-                    lastPrice,
-
-                    " ",
-
-                    bidUnit),
-
-                React.createElement("div", { className: "pg-trading-header-price-bar-filler-right", style: { width: `${100 - percentage}%` } }))));
-
-    }
-
+interface Props {
+    percentage: number;
+    lastPrice?: string;
+    bidUnit?: string;
 }
+
+export class PriceBar extends React.Component<Props> {
+    public render() {
+        const {percentage, lastPrice, bidUnit} = this.props;
+        const gap = 18;
+        const position = percentage > 50 ? 'right' : 'left';
+        let positionValue;
+        positionValue = position === 'left' ? percentage < gap ? '0' : `${percentage - gap}%` : percentage + gap > 100 ? '0' : `${100 - percentage - gap}%`;
+        const style = {};
+        style[position] = positionValue;
+        return (
+            <div className="pg-trading-header-price-bar">
+                <div className="pg-trading-header-price-bar-filler">
+                    <div className="pg-trading-header-price-bar-filler-left" style={{width: `${percentage}%`}}/>
+                    <div className="pg-trading-header-price-bar-filler-cursor"/>
+                    <div className="pg-trading-header-price-bar-text" style={style}>Last Price: {lastPrice} {bidUnit}</div>
+                    <div
+                        className="pg-trading-header-price-bar-filler-right"
+                        style={{width: `${100 - percentage}%`}}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/containers/ToolBar/PriceBar/index.tsx
+
+
+
+// WEBPACK FOOTER //
+// ./src/containers/ToolBar/PriceBar/index.tsx
