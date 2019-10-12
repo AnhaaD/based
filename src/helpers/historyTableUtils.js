c07761b24697504ba79bc7139c7fea1568490a1d
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:10:30 +0100

    Update historyTableUtils.js

--- src/helpers/historyTableUtils.js
+++ src/helpers/historyTableUtils.js
@@ -1,63 +1,46 @@
 export const depositColorMapping = {
-
     accepted: 'var(--color-green)',
-
     collected: 'var(--color-green)',
-
     submitted: '',
-
     canceled: 'var(--color-red)',
-
     rejected: 'var(--color-red)',
-
 };
 
 export const withdrawColorMapping = {
-
     prepared: '',
-
     submitted: '',
-
     canceled: 'var(--color-red)',
-
     accepted: 'var(--color-green)',
-
     suspected: '',
-
     rejected: 'var(--color-red)',
-
     processing: '',
-
     succeed: 'var(--color-green)',
-
     failed: 'var(--color-red)',
-
     confirming: '',
-
 };
 
 export const tradesColorMapping = {
-
     ask: {
-
         color: 'var(--color-red)',
-
         text: 'Sell',
-
     },
-
     bid: {
-
         color: 'var(--color-green)',
-
         text: 'Buy',
-
     },
-
 };
 
-export const setDepositStatusColor = (status) => depositColorMapping[status];
+export const setDepositStatusColor = (status: string): string => depositColorMapping[status];
 
-export const setWithdrawStatusColor = (status) => withdrawColorMapping[status];
+export const setWithdrawStatusColor = (status: string): string => withdrawColorMapping[status];
 
-export const setTradesType = (type) => tradesColorMapping[type] || { color: '', text: '' };
+export const setTradesType = (type: string) => tradesColorMapping[type] || { color: '', text: '' };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/helpers/historyTableUtils.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/helpers/historyTableUtils.ts
