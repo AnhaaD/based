4d4e86d7462a8983463651963e4cd717565426bb
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:33:54 +0100

    Update userOpenOrdersFetchSaga.js

--- src/modules/user/openOrders/sagas/userOpenOrdersFetchSaga.js
+++ src/modules/user/openOrders/sagas/userOpenOrdersFetchSaga.js
@@ -1,35 +1,27 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../public/alert';
+import {
+    userOpenOrdersData,
+    userOpenOrdersError,
+    UserOpenOrdersFetch,
+} from '../actions';
 
-import { userOpenOrdersData, userOpenOrdersError, } from '../actions';
 
-const ordersOptions = {
-
+const ordersOptions: RequestOptions = {
     apiVersion: 'peatio',
-
 };
 
-export function* userOpenOrdersFetchSaga(action) {
-
+export function* userOpenOrdersFetchSaga(action: UserOpenOrdersFetch) {
     try {
-
         const { market } = action.payload;
-
         const list = yield call(API.get(ordersOptions), `/market/orders?market=${market.id}&state=wait`);
 
         yield put(userOpenOrdersData(list));
-
-    }
-
-    catch (error) {
-
+    } catch (error) {
         yield put(userOpenOrdersError());
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
+        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
     }
-
 }
+
