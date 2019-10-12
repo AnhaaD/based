ddb4f25069394a0381edf258d4d764e86c5d9241
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-13 00:01:08 +0100

    Update reducer.js

--- src/modules/user/userActivity/reducer.js
+++ src/modules/user/userActivity/reducer.js
@@ -1,69 +1,61 @@
-import { USER_ACTIVITY_DATA, USER_ACTIVITY_ERROR, USER_ACTIVITY_FETCH, } from './constants';
+import { CommonError } from '../../types';
+import { UserActivityAction, UserActivityDataInterface } from './actions';
+import {
+    USER_ACTIVITY_DATA,
+    USER_ACTIVITY_ERROR,
+    USER_ACTIVITY_FETCH,
+} from './constants';
 
-export const initialUserActivityState = {
 
+export interface UserActivityState {
+    loading: boolean;
+    page: number;
+    total: number;
+    list: UserActivityDataInterface[];
+    error?: CommonError;
+}
+
+export const initialUserActivityState: UserActivityState = {
     list: [],
-
     loading: false,
-
     page: 0,
-
     total: 0,
-
 };
 
-export const userActivityReducer = (state = initialUserActivityState, action) => {
-
+export const userActivityReducer = (state = initialUserActivityState, action: UserActivityAction) => {
     switch (action.type) {
-
         case USER_ACTIVITY_FETCH:
-
             return {
-
                 ...state,
-
                 loading: true,
-
             };
-
         case USER_ACTIVITY_DATA:
-
             return {
-
                 ...state,
-
                 list: action.payload.list,
-
                 loading: false,
-
                 page: action.payload.page,
-
                 total: action.payload.total,
-
             };
-
         case USER_ACTIVITY_ERROR:
-
             return {
-
                 ...state,
-
                 error: action.payload,
-
                 list: [],
-
                 loading: false,
-
                 page: 0,
-
                 total: 0,
-
             };
-
         default:
-
             return state;
-
     }
-
 };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/userActivity/reducer.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/userActivity/reducer.ts
