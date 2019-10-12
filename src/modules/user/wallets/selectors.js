d4faa33fd2c147125d88231e3d580ddd50317304
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-13 00:07:34 +0100

    Update selectors.js

--- src/modules/user/wallets/selectors.js
+++ src/modules/user/wallets/selectors.js
@@ -1,9 +1,19 @@
-export const selectWallets = (state) => state.user.wallets.wallets.list;
+import { RootState } from '../../';
+import { CommonError } from '../../types';
+import { Wallet } from './types';
 
-export const selectWalletsLoading = (state) => state.user.wallets.wallets.loading;
+export const selectWallets = (state: RootState): Wallet[] =>
+    state.user.wallets.wallets.list;
 
-export const selectWithdrawSuccess = (state) => state.user.wallets.wallets.withdrawSuccess;
+export const selectWalletsLoading = (state: RootState): boolean =>
+    state.user.wallets.wallets.loading;
 
-export const selectWalletsAddressError = (state) => state.user.wallets.wallets.error;
+export const selectWithdrawSuccess = (state: RootState): boolean =>
+    state.user.wallets.wallets.withdrawSuccess;
 
-export const selectMobileWalletUi = (state) => state.user.wallets.wallets.mobileWalletChosen;
+export const selectWalletsAddressError = (state: RootState): CommonError | undefined =>
+    state.user.wallets.wallets.error;
+
+export const selectMobileWalletUi = (state: RootState): string =>
+    state.user.wallets.wallets.mobileWalletChosen;
+
