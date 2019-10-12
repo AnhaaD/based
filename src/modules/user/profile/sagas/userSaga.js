2008a79f8af99d19d756e37835acaa0ed3242211
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:56:39 +0100

    Update userSaga.js

--- src/modules/user/profile/sagas/userSaga.js
+++ src/modules/user/profile/sagas/userSaga.js
@@ -1,35 +1,25 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
+import { API, RequestOptions } from '../../../../api';
+import {
+    userData,
+    userError,
+} from '../actions';
 
-import { API } from '../../../../api';
-
-import { userData, userError, } from '../actions';
-
-const userOptions = {
-
+const userOptions: RequestOptions = {
     apiVersion: 'barong',
-
 };
 
 export function* userSaga() {
-
     try {
-
         const user = yield call(API.get(userOptions), '/resource/users/me');
-
         const payload = {
-
             user: user,
-
         };
-
         yield put(userData(payload));
-
-    }
-
-    catch (error) {
-
+    } catch (error) {
         yield put(userError(error));
-
     }
-
 }
+
+
