728fb8ff01233fe1c80721fc1924e8a7b46b1392
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-13 00:12:22 +0100

    Update walletsAddressSaga.js

--- src/modules/user/wallets/sagas/walletsAddressSaga.js
+++ src/modules/user/wallets/sagas/walletsAddressSaga.js
@@ -1,43 +1,32 @@
+// tslint:disable-next-line:no-submodule-imports
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../public/alert';
+import {
+    walletsAddressData,
+    walletsAddressError,
+    WalletsAddressFetch,
+} from '../actions';
 
-import { walletsAddressData, walletsAddressError, } from '../actions';
-
-const walletsAddressOptions = {
-
+const walletsAddressOptions: RequestOptions = {
     apiVersion: 'peatio',
-
 };
 
-export function* walletsAddressSaga(action) {
-
+export function* walletsAddressSaga(action: WalletsAddressFetch) {
     try {
-
         const currency = action.payload.currency.toLocaleLowerCase();
-
         const url = `/account/deposit_address/${currency}`;
-
         const { address } = yield call(API.get(walletsAddressOptions), url);
 
         yield put(walletsAddressData({
-
             address,
-
             currency,
-
         }));
-
-    }
-
-    catch (error) {
-
+    } catch (error) {
         yield put(walletsAddressError(error));
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
+        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
     }
-
 }
+
+
+
