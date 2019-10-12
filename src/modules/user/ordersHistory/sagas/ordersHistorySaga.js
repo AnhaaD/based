5017e1b5e6590dd060f65ae523d5a7f3c71da2ba
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:46:34 +0100

    Update ordersHistorySaga.js

--- src/modules/user/ordersHistory/sagas/ordersHistorySaga.js
+++ src/modules/user/ordersHistory/sagas/ordersHistorySaga.js
@@ -1,43 +1,39 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-
 import { alertPush } from '../../..';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { convertOrderAPI } from '../../openOrders/helpers';
+import {
+    userOrdersHistoryData,
+    userOrdersHistoryError,
+    UserOrdersHistoryFetch,
+} from '../actions';
 
-import { userOrdersHistoryData, userOrdersHistoryError, } from '../actions';
-
-const ordersOptions = {
-
+const ordersOptions: RequestOptions = {
     apiVersion: 'peatio',
-
     withHeaders: true,
-
 };
 
-export function* ordersHistorySaga(action) {
-
+export function* ordersHistorySaga(action: UserOrdersHistoryFetch) {
     try {
-
         const { pageIndex, limit, type } = action.payload;
-
         const params = `limit=${limit}&page=${pageIndex + 1}${type === 'all' ? '' : '&state=wait'}`;
-
         const { data, headers } = yield call(API.get(ordersOptions), `/market/orders?${params}`);
 
         const list = data.map(convertOrderAPI);
 
         yield put(userOrdersHistoryData({ list, pageIndex, total: headers.total }));
-
-    }
-
-    catch (error) {
-
+    } catch (error) {
         yield put(userOrdersHistoryError());
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
+        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
     }
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/ordersHistory/sagas/ordersHistorySaga.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/ordersHistory/sagas/ordersHistorySaga.ts
