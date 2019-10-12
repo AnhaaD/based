bc849a624bd47a79f24a515914d546870c21ba85
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:56:03 +0100

    Update toggle2faSaga.js

--- src/modules/user/profile/sagas/toggle2faSaga.js
+++ src/modules/user/profile/sagas/toggle2faSaga.js
@@ -1,37 +1,26 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../public/alert';
+import {
+    toggle2faData,
+    toggle2faError,
+    Toggle2FAFetch,
+} from '../actions';
 
-import { toggle2faData, toggle2faError, } from '../actions';
-
-const enable2faOptions = {
-
+const enable2faOptions: RequestOptions = {
     apiVersion: 'barong',
-
 };
 
-export function* toggle2faSaga(action) {
-
+export function* toggle2faSaga(action: Toggle2FAFetch) {
     try {
-
         const code = { code: action.payload.code };
-
         yield call(API.post(enable2faOptions), '/resource/otp/enable', code);
-
         yield put(toggle2faData());
-
-        yield put(alertPush({ message: ['success.otp.enabled'], type: 'success' }));
-
-    }
-
-    catch (error) {
-
+        yield put(alertPush({message: ['success.otp.enabled'], type: 'success'}));
+    } catch (error) {
         yield put(toggle2faError(error));
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
+        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
     }
-
 }
+
