0a8d2598c5eeb7c38b57916761c48ffd489b0800
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:04:07 +0100

    Update calcMaxVolume.js

--- src/helpers/calcMaxVolume.js
+++ src/helpers/calcMaxVolume.js
@@ -1,7 +1,6 @@
 import { accumulateVolume } from './accumulateVolume';
 
-export const calcMaxVolume = (bids, asks) => {
-
+export const calcMaxVolume = (bids: string[][], asks: string[][]) => {
     return Math.max(...accumulateVolume(bids), ...accumulateVolume(asks));
-
 };
+
