72f47b4a5a8cac9cbf2b06c1a8a30cdddb83eaf5
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:53:05 +0100

    Update changePasswordSaga.js

--- src/modules/user/profile/sagas/changePasswordSaga.js
+++ src/modules/user/profile/sagas/changePasswordSaga.js
@@ -1,35 +1,26 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../public/alert';
+import {
+    changePasswordData,
+    changePasswordError,
+    ChangePasswordFetch,
+} from '../actions';
 
-import { changePasswordData, changePasswordError, } from '../actions';
-
-const changePasswordOptions = {
-
+const changePasswordOptions: RequestOptions = {
     apiVersion: 'barong',
-
 };
 
-export function* changePasswordSaga(action) {
-
+export function* changePasswordSaga(action: ChangePasswordFetch) {
     try {
-
         yield call(API.put(changePasswordOptions), '/resource/users/password', action.payload);
-
         yield put(changePasswordData());
-
-        yield put(alertPush({ message: ['success.password.changed'], type: 'success' }));
-
-    }
-
-    catch (error) {
-
+        yield put(alertPush({message: ['success.password.changed'], type: 'success'}));
+    } catch (error) {
         yield put(changePasswordError(error));
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
+        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
     }
-
 }
+
+
