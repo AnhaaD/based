2e3a40c4840f81d5aa809b4395918fd205e65178
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:33:16 +0100

    Update openOrdersCancelSaga.js

--- src/modules/user/openOrders/sagas/openOrdersCancelSaga.js
+++ src/modules/user/openOrders/sagas/openOrdersCancelSaga.js
@@ -1,39 +1,27 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../public/alert';
+import { openOrdersCancelData, openOrdersCancelError, OpenOrdersCancelFetch } from '../actions';
 
-import { openOrdersCancelData, openOrdersCancelError } from '../actions';
 
-const ordersCancelOptions = {
-
+const ordersCancelOptions: RequestOptions = {
     apiVersion: 'peatio',
-
 };
 
-export function* openOrdersCancelSaga(action) {
-
+export function* openOrdersCancelSaga(action: OpenOrdersCancelFetch) {
     try {
-
         const { id, list } = action.payload;
-
         yield call(API.post(ordersCancelOptions), `/market/orders/${id}/cancel`, { id });
 
         const updatedList = list.filter(order => order.id !== id);
 
         yield put(openOrdersCancelData(updatedList));
-
-        yield put(alertPush({ message: ['success.order.canceled'], type: 'success' }));
-
-    }
-
-    catch (error) {
-
+        yield put(alertPush({ message: ['success.order.canceled'], type: 'success'}));
+    } catch (error) {
         yield put(openOrdersCancelError());
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
+        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
     }
-
 }
+
+
