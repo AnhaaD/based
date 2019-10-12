2af5e387fc35a129a02bcd06688b8d83c4d403a9
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:51:22 +0100

    Update reducer.js

--- src/modules/user/password/reducer.js
+++ src/modules/user/password/reducer.js
@@ -1,67 +1,53 @@
-import { PASSWORD_CHANGE_FORGOT_PASSWORD_FETCH, PASSWORD_CHANGE_FORGOT_PASSWORD_SUCCESS, PASSWORD_FORGOT_ERROR, PASSWORD_FORGOT_FETCH, PASSWORD_FORGOT_SUCCESS, } from './constants';
+import { CommonError, CommonState } from '../../types';
+import { PasswordAction } from './actions';
+import {
+    PASSWORD_CHANGE_FORGOT_PASSWORD_FETCH,
+    PASSWORD_CHANGE_FORGOT_PASSWORD_SUCCESS,
+    PASSWORD_FORGOT_ERROR,
+    PASSWORD_FORGOT_FETCH,
+    PASSWORD_FORGOT_SUCCESS,
+} from './constants';
 
-const initialState = {
+export interface PasswordState extends CommonState {
+    forgotPasswordRequested: boolean;
+    forgotPasswordChanged: boolean;
+    forgotPasswordError?: CommonError;
+    loading?: boolean;
+}
 
+const initialState: PasswordState = {
     loading: false,
-
     forgotPasswordRequested: false,
-
     forgotPasswordChanged: false,
-
 };
 
-export const passwordReducer = (state = initialState, action) => {
-
+export const passwordReducer = (state = initialState, action: PasswordAction) => {
     switch (action.type) {
-
         case PASSWORD_FORGOT_FETCH:
-
             return { ...state, loading: true };
-
         case PASSWORD_FORGOT_SUCCESS:
-
             return {
-
                 ...state,
-
                 forgotPasswordRequested: true,
-
                 loading: false,
-
             };
-
         case PASSWORD_FORGOT_ERROR:
-
             return {
-
                 ...state,
-
                 forgotPasswordError: action.payload,
-
                 loading: false,
-
             };
-
         case PASSWORD_CHANGE_FORGOT_PASSWORD_FETCH:
-
             return { ...state, loading: true };
-
         case PASSWORD_CHANGE_FORGOT_PASSWORD_SUCCESS:
-
             return {
-
                 ...state,
-
                 forgotPasswordChanged: true,
-
                 loading: false,
-
             };
-
         default:
-
             return state;
-
     }
-
 };
+
+
