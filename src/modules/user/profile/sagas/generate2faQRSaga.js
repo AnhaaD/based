8b590719ce0e60951178371d66f41306d1a0268a
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:53:56 +0100

    Update generate2faQRSaga.js

--- src/modules/user/profile/sagas/generate2faQRSaga.js
+++ src/modules/user/profile/sagas/generate2faQRSaga.js
@@ -1,35 +1,28 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../public/alert';
-
 import { generate2faQRData, generate2faQRError } from '../actions';
 
-const generate2faQROptions = {
-
+const generate2faQROptions: RequestOptions = {
     apiVersion: 'barong',
-
 };
 
-export function* generate2faQRSaga() {
-
-    try {
-
-        const qrData = yield call(API.post(generate2faQROptions), '/resource/otp/generate_qrcode');
-
-        const { barcode, url } = qrData.data;
-
-        yield put(generate2faQRData({ barcode, url }));
-
-    }
-
-    catch (error) {
-
-        yield put(generate2faQRError(error));
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
-    }
-
+interface GenerateQRResponse {
+    data: {
+        barcode: string;
+        url: string;
+    };
 }
+
+export function* generate2faQRSaga() {
+    try {
+        const qrData: GenerateQRResponse = yield call(API.post(generate2faQROptions), '/resource/otp/generate_qrcode');
+        const { barcode, url } = qrData.data;
+        yield put(generate2faQRData({ barcode, url }));
+    } catch (error) {
+        yield put(generate2faQRError(error));
+        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
+    }
+}
+
