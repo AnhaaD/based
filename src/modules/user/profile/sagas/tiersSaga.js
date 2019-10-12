d754ff5b5c3296d28d16dbe2e76b2882c0a27ef5
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:55:17 +0100

    Update tiersSaga.js

--- src/modules/user/profile/sagas/tiersSaga.js
+++ src/modules/user/profile/sagas/tiersSaga.js
@@ -1,43 +1,30 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
+import { API, RequestOptions } from '../../../../api';
+import {
+    tiersData,
+    tiersDisable,
+    tiersError,
+    TiersFetch,
+} from '../actions';
 
-import { API } from '../../../../api';
-
-import { tiersData, tiersDisable, tiersError, } from '../actions';
-
-const tiersOptions = {
-
+const tiersOptions: RequestOptions = {
     apiVersion: 'applogic',
-
 };
 
-export function* tiersSaga(action) {
-
+export function* tiersSaga(action: TiersFetch) {
     try {
-
         const { uid, currency } = action.payload;
-
         const tier = yield call(API.get(tiersOptions), `/tiers/${uid}?currency=${currency}`);
-
         yield put(tiersData(tier));
-
-    }
-
-    catch (error) {
-
+    } catch (error) {
         const tiersDisabled = error.code === 204;
-
         if (tiersDisabled) {
-
             yield put(tiersDisable());
-
-        }
-
-        else {
-
+        } else {
             yield put(tiersError(error));
-
         }
-
     }
-
 }
+
+
