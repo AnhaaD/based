8a6b8ae5b4022b654aceec01b6fb793728dd63f4
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-13 00:04:16 +0100

    Update index.js

--- src/modules/user/userActivity/sagas/index.js
+++ src/modules/user/userActivity/sagas/index.js
@@ -1,11 +1,10 @@
+// tslint:disable-next-line
 import { takeEvery } from 'redux-saga/effects';
-
-import { USER_ACTIVITY_FETCH, } from '../constants';
-
+import {
+    USER_ACTIVITY_FETCH,
+} from '../constants';
 import { userActivitySaga } from './userActivitySaga';
 
 export function* rootUserActivitySaga() {
-
     yield takeEvery(USER_ACTIVITY_FETCH, userActivitySaga);
-
 }
