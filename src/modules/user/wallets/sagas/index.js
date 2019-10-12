e4543004065d28228110e8389bc3dad9d1842f9a
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-13 00:11:44 +0100

    Update index.js

--- src/modules/user/wallets/sagas/index.js
+++ src/modules/user/wallets/sagas/index.js
@@ -1,20 +1,17 @@
-import * as React from 'react';
+// tslint:disable-next-line:no-submodule-imports
 import { takeEvery } from 'redux-saga/effects';
-
-import { WALLETS_ADDRESS_FETCH, WALLETS_FETCH, WALLETS_WITHDRAW_CCY_FETCH, } from '../constants';
-
+import {
+    WALLETS_ADDRESS_FETCH,
+    WALLETS_FETCH,
+    WALLETS_WITHDRAW_CCY_FETCH,
+} from '../constants';
 import { walletsAddressSaga } from './walletsAddressSaga';
-
 import { walletsSaga } from './walletsSaga';
-
 import { walletsWithdrawCcySaga } from './walletsWithdrawSaga';
 
 export function* rootWalletsSaga() {
-
     yield takeEvery(WALLETS_FETCH, walletsSaga);
-
     yield takeEvery(WALLETS_ADDRESS_FETCH, walletsAddressSaga);
-
     yield takeEvery(WALLETS_WITHDRAW_CCY_FETCH, walletsWithdrawCcySaga);
-
 }
+
