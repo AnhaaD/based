9b865bd906caa9a616259c2eccd12dcda03c378b
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:08:15 +0100

    Update getLanguageByCode.js

--- src/helpers/getLanguageByCode.js
+++ src/helpers/getLanguageByCode.js
@@ -1,12 +1,21 @@
-export const getLanguageName = (code) => {
-    switch (code) {
-        case 'en':
-            return 'English';
-        case 'ru':
-            return 'Русский';
-        case 'zh':
-            return '中国';
-        default:
-            return 'English';
-    }
+export const getLanguageName = (code: string) => {
+    switch (code) {
+        case 'en':
+            return 'English';
+        case 'ru':
+            return 'Русский';
+        case 'zh':
+            return '中国';
+        default:
+            return 'English';
+    }
 };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/helpers/getLanguageByCode.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/helpers/getLanguageByCode.ts
