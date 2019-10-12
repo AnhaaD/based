250737b1a75d4584233f667b5809c459ad05aa01
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:08:52 +0100

    Update getTotalPrice.js

--- src/helpers/getTotalPrice.js
+++ src/helpers/getTotalPrice.js
@@ -1,93 +1,61 @@
-export const getTotalPrice = (amount, proposals) => {
+type FormType = 'buy' | 'sell';
 
-    for (const p of proposals) {
-
-        if (p.length < 2) {
-
-            return 0;
-
-        }
-
-    }
-
+export const getTotalPrice = (amount: string, proposals: string[][], type: FormType) => {
     let sum = Number(amount);
 
-    const list = proposals;
-
+    const list = type === 'buy' ? proposals : proposals.slice(0).reverse();
     let total = 0;
 
     for (const proposal of list) {
-
         if (!sum) {
-
             break;
-
         }
 
         if (sum <= Number(proposal[1])) {
-
             total += sum * Number(proposal[0]);
-
             sum = 0;
-
-        }
-
-        else {
-
+        } else {
             total += Number(proposal[1]) * Number(proposal[0]);
-
             sum -= Number(proposal[1]);
-
         }
-
     }
 
-    if (sum > 0 && list.length >= 1) { // sum is bigger then order book liqudity
-
+    if (sum > 0) { // sum is bigger then order book liqudity
         const lastPrice = Number(list[list.length - 1][0]);
-
         total += lastPrice * sum;
-
     }
 
     return total;
-
 };
 
-export const getAmount = (avaiblePrice, proposals, value) => {
-
+export const getAmount = (avaiblePrice: number, proposals: string[][], type: FormType, value: number) => {
     let sum = avaiblePrice * value;
 
-    const list = proposals;
-
+    const list = type === 'buy' ? proposals : proposals.slice(0).reverse();
     let totalAmount = 0;
 
     for (const proposal of list) {
-
         if (!sum) {
-
             break;
-
         }
 
         if (sum <= (Number(proposal[0]) * Number(proposal[1]))) {
-
             totalAmount += sum / Number(proposal[0]);
-
             sum = 0;
-
-        }
-
-        else {
-
+        } else {
             totalAmount += Number(proposal[1]);
-
             sum -= (Number(proposal[0]) * Number(proposal[1]));
-
         }
-
     }
 
     return totalAmount;
-
 };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/helpers/getTotalPrice.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/helpers/getTotalPrice.ts
