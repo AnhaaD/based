c666d1b0fe939027b8009da27c8e30eae909aa14
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:48:22 +0100

    Update forgotPasswordSaga.js

--- src/modules/user/password/sagas/forgotPasswordSaga.js
+++ src/modules/user/password/sagas/forgotPasswordSaga.js
@@ -1,35 +1,26 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../public/alert';
+import {
+    forgotPasswordError,
+    ForgotPasswordFetch,
+    forgotPasswordSuccess,
+} from '../actions';
 
-import { forgotPasswordError, forgotPasswordSuccess, } from '../actions';
-
-const forgotPasswordConfig = {
-
+const forgotPasswordConfig: RequestOptions = {
     apiVersion: 'barong',
-
 };
 
-export function* forgotPasswordSaga(action) {
-
+export function* forgotPasswordSaga(action: ForgotPasswordFetch) {
     try {
-
         yield call(API.post(forgotPasswordConfig), '/identity/users/password/generate_code', action.payload);
-
         yield put(forgotPasswordSuccess());
-
-        yield put(alertPush({ message: ['success.password.forgot'], type: 'success' }));
-
-    }
-
-    catch (error) {
-
+        yield put(alertPush({message: ['success.password.forgot'], type: 'success'}));
+    } catch (error) {
         yield put(forgotPasswordError(error));
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
+        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
     }
-
 }
+
+
