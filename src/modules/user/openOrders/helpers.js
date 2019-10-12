9c019bdd11875a2af0a5ec7fcdd6de2803a6d575
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:29:31 +0100

    Update helpers.js

--- src/modules/user/openOrders/helpers.js
+++ src/modules/user/openOrders/helpers.js
@@ -1,117 +1,78 @@
 import { kindToMakerType } from '../../helpers';
+import { OrderAPI, OrderCommon, OrderEvent } from '../../types';
 
-export const convertOrderAPI = (order) => {
-
-    const { id, side, price, state, created_at, remaining_volume, origin_volume, executed_volume, market, ord_type, avg_price, updated_at, } = order;
-
-    return {
-
+export const convertOrderAPI = (order: OrderAPI): OrderCommon => {
+    const {
         id,
-
         side,
-
-        price: Number(price),
-
+        price,
         state,
-
         created_at,
-
-        origin_volume: Number(origin_volume),
-
-        remaining_volume: Number(remaining_volume),
-
-        executed_volume: Number(executed_volume),
-
+        remaining_volume,
+        origin_volume,
+        executed_volume,
         market,
-
         ord_type,
-
-        avg_price: Number(avg_price),
-
+        avg_price,
         updated_at,
-
-    };
-
-};
-
-export const convertOrderEvent = (orderEvent) => {
-
-    const { id, at, kind, price, state, remaining_volume, origin_volume, market } = orderEvent;
-
+    } = order;
     return {
-
         id,
-
-        side: kindToMakerType(kind),
-
+        side,
         price: Number(price),
-
         state,
-
-        remaining_volume: Number(remaining_volume),
-
-        executed_volume: Number(origin_volume) - Number(remaining_volume),
-
+        created_at,
         origin_volume: Number(origin_volume),
-
-        created_at: new Date(Number(at) * 1000).toISOString(),
-
+        remaining_volume: Number(remaining_volume),
+        executed_volume: Number(executed_volume),
         market,
-
+        ord_type,
+        avg_price: Number(avg_price),
+        updated_at,
     };
-
 };
 
-export const insertOrUpdate = (list, order) => {
+export const convertOrderEvent = (orderEvent: OrderEvent): OrderCommon => {
+    const { id, at, kind, price, state, remaining_volume, origin_volume, market } = orderEvent;
+    return {
+        id,
+        side: kindToMakerType(kind),
+        price: Number(price),
+        state,
+        remaining_volume: Number(remaining_volume),
+        executed_volume: Number(origin_volume) - Number(remaining_volume),
+        origin_volume: Number(origin_volume),
+        created_at: new Date(Number(at) * 1000).toISOString(),
+        market,
+    };
+};
 
+export const insertOrUpdate = (list: OrderCommon[], order: OrderCommon): OrderCommon[] => {
     const { state, id } = order;
-
     switch (state) {
-
         case 'wait':
-
-            const index = list.findIndex((value) => value.id === id);
-
+            const index = list.findIndex((value: OrderCommon) => value.id === id);
             if (index === -1) {
-
-                return list.concat({ ...order });
-
+                return list.concat({...order});
             }
-
             return list.map(item => {
-
                 if (item.id === order.id) {
-
-                    return { ...order };
-
+                    return {...order};
                 }
-
                 return item;
-
             });
-
         default:
-
-            return list.reduce((memo, item) => {
-
+            return list.reduce((memo: OrderCommon[], item: OrderCommon): OrderCommon[] => {
                 if (id !== item.id) {
-
                     memo.push(item);
-
                 }
-
                 return memo;
-
             }, []);
-
     }
-
 };
 
-export const insertIfNotExisted = (list, order) => {
-
-    const index = list.findIndex((value) => value.id === order.id);
-
-    return (index === -1) ? list.concat({ ...order }) : [...list];
-
+export const insertIfNotExisted = (list: OrderCommon[], order: OrderCommon): OrderCommon[] => {
+    const index = list.findIndex((value: OrderCommon) => value.id === order.id);
+    return (index === -1) ? list.concat({...order}) : [...list];
 };
+
