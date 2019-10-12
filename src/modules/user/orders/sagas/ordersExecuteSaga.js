b4ca80445df53e1abf4b2c0de1f675a451daad6e
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:39:27 +0100

    Update ordersExecuteSaga.js

--- src/modules/user/orders/sagas/ordersExecuteSaga.js
+++ src/modules/user/orders/sagas/ordersExecuteSaga.js
@@ -1,43 +1,37 @@
+// tslint:disable-next-line
 import { call, put } from 'redux-saga/effects';
-
-import { API } from '../../../../api';
-
+import { API, RequestOptions } from '../../../../api';
 import { alertPush } from '../../../public/alert';
-
 import { userOpenOrdersAppend } from '../../openOrders';
+import {
+    orderExecuteData,
+    orderExecuteError,
+    OrderExecuteFetch,
+} from '../actions';
 
-import { orderExecuteData, orderExecuteError, } from '../actions';
-
-const executeOptions = {
-
+const executeOptions: RequestOptions = {
     apiVersion: 'peatio',
-
 };
 
-export function* ordersExecuteSaga(action) {
-
+export function* ordersExecuteSaga(action: OrderExecuteFetch) {
     try {
-
         const order = yield call(API.post(executeOptions), '/market/orders', action.payload);
-
         yield put(orderExecuteData());
-
         if (order.ord_type !== 'market') {
-
             yield put(userOpenOrdersAppend(order));
-
         }
-
-        yield put(alertPush({ message: ['success.order.created'], type: 'success' }));
-
-    }
-
-    catch (error) {
-
+        yield put(alertPush({ message: ['success.order.created'], type: 'success'}));
+    } catch (error) {
         yield put(orderExecuteError(error));
-
-        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
-
+        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
     }
-
 }
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/modules/user/orders/sagas/ordersExecuteSaga.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/modules/user/orders/sagas/ordersExecuteSaga.ts
