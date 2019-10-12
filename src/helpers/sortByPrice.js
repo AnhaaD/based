de8d3adfaa4e3a69716db3e55d66c99da182e22b
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:19:29 +0100

    Update sortByPrice.js

--- src/helpers/sortByPrice.js
+++ src/helpers/sortByPrice.js
@@ -1,11 +1,8 @@
-export const sortAsks = (asks) => {
-
+export const sortAsks = (asks: string[][]) => {
     return asks.sort((a, b) => Number(a[0]) - Number(b[0]));
-
 };
 
-export const sortBids = (bids) => {
-
+export const sortBids = (bids: string[][]) => {
     return bids.sort((a, b) => Number(b[0]) - Number(a[0]));
-
 };
+
