31075f58f756385034caaec6419bf42649cd040e
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:18:49 +0100

    Update sortByDate.js

--- src/helpers/sortByDate.js
+++ src/helpers/sortByDate.js
@@ -1,13 +1,8 @@
 import * as moment from 'moment';
-
 import { localeDate } from './localeDate';
 
-export const sortByDate = (prop, format) => {
-
-    return (a, b) => {
-
-        return moment(localeDate(a[prop]), format) > moment(localeDate(b[prop]), format) ? -1 : 1;
-
-    };
-
+export const sortByDate = (prop: string, format: string) => {
+  return (a, b) => {
+      return moment(localeDate(a[prop], 'fullDate'), format) > moment(localeDate(b[prop], 'fullDate'), format) ? -1 : 1;
+  };
 };
