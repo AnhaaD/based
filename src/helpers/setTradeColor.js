49d5885fe5d9d8725ff9b67dcc2938347c46be33
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:17:19 +0100

    Update setTradeColor.js

--- src/helpers/setTradeColor.js
+++ src/helpers/setTradeColor.js
@@ -1,17 +1,12 @@
 const tradesColorMapping = {
-
     buy: {
-
         color: 'var(--color-green)',
-
     },
-
     sell: {
-
         color: 'var(--color-red)',
-
     },
-
 };
 
-export const setTradeColor = (side) => tradesColorMapping[side] || { color: '' };
+export const setTradeColor = (side: string) => tradesColorMapping[side] || { color: ''};
+
+
