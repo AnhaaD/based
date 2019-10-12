a8ea52064c66090dea48bdb857eb3e3ac186a164
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:07:33 +0100

    Update filterData.js

--- src/helpers/filterData.js
+++ src/helpers/filterData.js
@@ -1,11 +1,16 @@
 const consist = (a, b) => {
-
     return a.toLowerCase().indexOf(b.toLowerCase()) !== -1;
-
 };
 
 export const handleFilter = (item, term) => {
-
     return consist(item.currency, term);
-
 };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/helpers/filterData.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/helpers/filterData.ts
