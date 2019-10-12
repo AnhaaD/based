3351aece493d2a0e30635c4f2c40faa2051ec520
Coreagex <39203852+coreagex@users.noreply.github.com>
2019-10-12 23:03:23 +0100

    Update accumulateVolume.js

--- src/helpers/accumulateVolume.js
+++ src/helpers/accumulateVolume.js
@@ -1,19 +1,19 @@
 export const accumulateVolume = array => {
-
-    const total = [];
-
+    const total: number[] = [];
     array.map(item => {
-
         return item[1];
-
     }).reduce((accumulator, currentValue, currentIndex) => {
-
         total[currentIndex] = Number(accumulator) + Number(currentValue);
-
         return (Number(accumulator) + Number(currentValue));
-
     }, 0);
-
     return total;
-
 };
+
+
+// WEBPACK FOOTER //
+// src/drone/src/src/helpers/accumulateVolume.ts
+
+
+
+// WEBPACK FOOTER //
+// ./src/helpers/accumulateVolume.ts
